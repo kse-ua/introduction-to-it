b6ca068a7be2cef69186d6e48eab12737db9591a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-11-14 23:21:02 +0200

    Add loop examples

---
+++ lecture_notes/07 basics/2-for-loop.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int MAX_VALUE = 10;
+
+int main() {
+  printf("Begin\n");
+
+  for (int i = 0; i < MAX_VALUE; i++) {
+    printf("i: %d\n", i);
+  }
+
+  printf("The end\n");
+}
---
+++ lecture_notes/07 basics/2-for-loop.py
@@ -0,0 +1,8 @@
+MAX_VALUE = 10
+
+print('Begin')
+
+for i in range(MAX_VALUE):
+  print('i:', i)
+
+print('The end')
