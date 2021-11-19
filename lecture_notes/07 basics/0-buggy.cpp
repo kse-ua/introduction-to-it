24318d246445e8b6df37e1f1079dfa6f5f94566f
tonyblackbird <akruk@kse.org.ua>
2021-11-19 13:18:55 +0200

    I don't know

--- lecture_notes/07 basics/0-buggy.cpp
+++ lecture_notes/07 basics/0-buggy.cpp
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
 int main() {
   int firstNum = 2;
   int secondNum = 3;
   int sum = firstNum + secondNum;
-  printf("%d\n", sum);
+  int a = 0;
+  std::cin >> a;
+  std::cout << a+2;
+  return 0;
 }
