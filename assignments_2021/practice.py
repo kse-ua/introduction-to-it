b4abde36d573116830963a4df98e2ba81b7c6b1d
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-11-19 10:23:12 +0200

    Create practice.py

---
+++ assignments_2021/practice.py
@@ -0,0 +1,35 @@
+# Given code
+
+first_num = 2;
+secord_num = 3;
+sum = firstNum + secondNum;
+print(sum);
+
+# 1. Delete colons that appear in code.
+
+first_num = 2
+secord_num = 3
+sum = firstNum + secord_num
+print(sum)
+
+# 2. Change vars "firstNum" and "secondNum" to "first_num" and "second_num" to avoid error.
+
+first_num = 2
+secord_num = 3
+sum = first_num + secord_num
+print(sum)
+
+# 3. There is misspell in var "secord_num", thus change it to "second_num"
+
+first_num = 2
+second_num = 3
+sum = first_num + second_num
+print(sum)
+
+# 4. To improve quality of code and avoid errors, change name of var "sum" to "sum_num", 'cause 'cause "sum"
+#    is a builtin function in python
+
+first_num = 2
+second_num = 3
+sum_num = first_num + second_num
+print(sum_num)
