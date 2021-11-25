ce9d7fba2a61c39699d22f608e0bc791ab1cad69
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-11-25 16:28:51 +0200

    Update 2-function.py

--- lecture_notes/08 collections/2-function.py
+++ lecture_notes/08 collections/2-function.py
@@ -1,12 +1,16 @@
-# Implementation
+# Python Code:
 
 def getFirstAndLast(array):
-  first = ages[0]
-  last = ages[-1]
-  return first, last
+    first = ages[0]
+    last = ages[-1]
 
-# Usage
+    return first, last
+
 
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first, last = getFirstAndLast(ages)
-print({ first, last })
+print(first, last)
+
+# JS code is pretty the same, however in JS code we can not set "-1" index, as we do in python, thus, we subtract 1 from
+# the len of an array. Of course, to print smth in JS we should use .log and semicolon after declaring a var.
+# Generally, JS has almost thr same syntax as PY.
