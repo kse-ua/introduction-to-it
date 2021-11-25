a4814edaa9e11248b457a5a154c3bae071a5dd1f
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-11-25 17:39:18 +0200

    Create 7-object.py

---
+++ lecture_notes/08 collections/7-object.py
@@ -0,0 +1,14 @@
+ages = {
+    'Vasia Pupkin': 19,
+    'Marcus Aurelius': 1860,
+}
+
+print(ages)
+
+ages['Vasia Pupkin'] = 20
+print(ages)
+
+del ages['Vasia Pupkin']
+print(ages)
+
+print('Vasia Pupkin:', 'Vasia Pupkin' in ages, 'Marcus Aurelius:', 'Marcus Aurelius' in ages)
