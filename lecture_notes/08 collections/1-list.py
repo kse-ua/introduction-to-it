ddfe2f8b6afb4d69e6bca9cc216715b119b21589
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-11-25 16:20:13 +0200

    Update 1-list.py

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,6 +1,16 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
-first = ages[0]
-last = ages[-1]
 
-print(first, last)
+# Add an integer to the TOP of the array
+ages.insert(0, 9)
+
+# Remove element from the TOP of the array
+del ages[0]
+
+# Add an integer to the END of array
+ages.append(21)
+
+# Remove element from the END of the array
+del ages[-1]
+
+print(ages)
