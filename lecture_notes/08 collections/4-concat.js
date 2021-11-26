7ca51231eccc6f96ada286cf0b6540ef8caa8da1
Stanislav Kramar <stanislavkramar@icloud.com>
2021-11-26 23:03:40 +0200

    optimize concat

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -3,20 +3,18 @@
 // Implementation
 
 const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
+    first: array[0],
+    last: array[array.length - 1],
 });
 
-const concat = (arr1, arr2) => {
-  const arr = arr1.slice();
-  arr.push(...arr2);
-  return arr;
-};
+const concat = (arr1, arr2) => arr1.concat(arr2);
 
 // Usage
 
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
+
 const ages = concat(schoolAges, studentAges);
-const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+const {first, last} = getFirstAndLast(ages);
+
+console.log({first, last});
