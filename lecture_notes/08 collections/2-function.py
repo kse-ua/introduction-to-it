2683823abb3b9ed133841ed8e00238577d195f00
Stanislav Kramar <stanislavkramar@icloud.com>
2021-11-28 02:41:10 +0200

    add comments describing differences between python and js implementations

--- lecture_notes/08 collections/2-function.py
+++ lecture_notes/08 collections/2-function.py
@@ -11,3 +11,8 @@ def get_first_and_last(array):
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first, last = get_first_and_last(ages)
 print({'first': first, 'last': last})
+
+# The two functions are pretty much the same except that return types are not equivalent: a tuple in the python version
+# and an object in the js one. And, also, python allows negative indexing which is prohibited in js that is why
+# ages[-1] is used instead of ages[len(ages) - 1]. Generally, in python for any sequence S, we can write w[-n]
+# where 0 <= n < len(S) <=> w[len(S) - n].
--- lecture_notes/08 collections/3-lambda.py
+++ lecture_notes/08 collections/3-lambda.py
@@ -7,3 +7,7 @@ get_first_and_last = lambda array: (array[0], array[-1])
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first, last = get_first_and_last(ages)
 print({'first': first, 'last': last})
+
+# While get_first_and_last lambda in python and getFirstAndLast function in the js version serve the same purpose,
+# their return types are not equivalent. The js version returns an object with two properties first and last.
+# In the python implementation, a tuple of 2 elements is returned.
