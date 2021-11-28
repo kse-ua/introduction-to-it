e2359826b4f9238a24b939b0d6648ba60063d4ac
Stanislav Kramar <stanislavkramar@icloud.com>
2021-11-28 03:13:06 +0200

    fix a typo

--- lecture_notes/08 collections/2-function.py
+++ lecture_notes/08 collections/2-function.py
@@ -14,5 +14,5 @@ print({'first': first, 'last': last})
 
 # The two functions are pretty much the same except that return types are not equivalent: a tuple in the python version
 # and an object in the js one. And, also, python allows negative indexing which is prohibited in js that is why
-# ages[-1] is used instead of ages[len(ages) - 1]. Generally, in python for any sequence S, we can write w[-n]
-# where 0 <= n < len(S) <=> w[len(S) - n].
+# ages[-1] is used instead of ages[len(ages) - 1]. Generally, in python for any sequence S, we can write S[-n]
+# where 0 <= n < len(S) <=> S[len(S) - n].
