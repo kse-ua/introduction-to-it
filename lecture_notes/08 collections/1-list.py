3d71ba2cc995dca76a362087b64b2c8f9683c139
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-11-30 13:28:51 +0200

    Update 1-list.py

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,12 +1,12 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-print("Initial array(list):", ages)
+print("Initial list:", ages)
 
 ages.pop(0)
 ages.append(25)
-ages.extend([30])     # Additional method i found, so I decided to demonstrate it
+ages.extend([30])     # Additional method I found, so I've decided to demonstrate it
 
 first = ages[0]
 last = ages[-1]
 
-print("Modified array(list):", ages)
+print("Modified array list:", ages)
 print({'first': first, 'last': last})
