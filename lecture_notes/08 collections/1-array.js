9499707613433ae9be7263cf10b15c3bdd8856bc
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-11-30 18:33:39 +0200

    array-methods-js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -5,4 +5,12 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const first = ages[0];
 const last = ages[ages.length - 1];
 
-console.log({ first, last }); 
+// removing/adding to the back
+ages.push(25);
+ages.pop();
+// removing/adding to the front
+ages.unshift(25);
+ages.shift();
+// reverse method 
+ages.reverse();
+console.log(ages)
