bbf3875b6d32fd028c8c00ca07e42503d80bb18e
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-11-30 13:54:55 +0200

    Used pop, push, shift, and unshift
    So as  can be seen, I added a lot of worded description to my code, so the changes are just a bit more visible when the code is executed. I mean it ain't necessary, if needed I can remove it)

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+console.log("Here's the initial version of our array:", ages);
 
 const first = ages[0];
 const last = ages[ages.length - 1];
+console.log("Let's see the original beginning and end",{ first, last }, '\n');
 
-console.log({ first, last });
+ages.pop();
+ages.push(21);
+console.log("Now we change the end of our array:", ages);
+
+ages.shift();
+ages.unshift(5);
+console.log("And now we change the start of our array:", ages);
+
+const newFirst = ages[0];
+const newLast = ages[ages.length - 1];
+
+console.log("\nAnd so let's see the new values:",{ newFirst, newLast });
