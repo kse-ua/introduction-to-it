21899b0ebcce2ff1c0e16c4604b3c408de121a88
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-11-30 17:19:00 +0200

    Created the translation from js of 6th example
    Translated from js to python, with regards to the fact that putting curly brackets around a variable's name in python doesn't print the name of the variable and the info stored inside it, unlike js

---
+++ lecture_notes/08 collections/6-set.py
@@ -0,0 +1,10 @@
+ages = {10, 12, 15, 15, 17, 18, 18, 19, 20}
+print(ages)
+
+ages.add(16)
+ages.discard(20)
+
+print("10:", 10 in ages, "  16:", 16 in ages, "  19:", 19 in ages, "  20:", 20 in ages)
+
+ages.clear()
+print(ages)
