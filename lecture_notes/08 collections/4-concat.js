da2bdc4bf7fd3626ac991dda10e17347a689e0e5
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:18:55 +0200

    Optimize JavaScript example

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -1,22 +1,14 @@
-'use strict';
-
 // Implementation
 
 const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
+    first: array[0],
+    last: array[array.length - 1],
 });
-
-const concat = (arr1, arr2) => {
-  const arr = arr1.slice();
-  arr.push(...arr2);
-  return arr;
-};
 
 // Usage
 
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
-const ages = concat(schoolAges, studentAges);
-const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+const ages = (schoolAges.concat(studentAges));
+const result = getFirstAndLast(ages)
+console.log(result)
