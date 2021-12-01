55d01c7f49f35ba8dc1a907659c2f5d35e2af2cc
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-01 10:47:40 +0200

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -2,6 +2,18 @@
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 
+// del last elem from the array
+ages.pop();
+
+// add elem at the end of the array
+ages.push(21);
+
+// del first elem from the list
+ages.shift();
+
+// add elem at the beggining of the list
+ages.unshift(9);
+
 const first = ages[0];
 const last = ages[ages.length - 1];
 
