5bd59b9b76dd81feef540a3db91043200525da0e
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-01 11:19:56 +0200

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,11 +1,11 @@
-let ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 ages.splice(4, 1);
-let first = ages[0];
-let last = ages[2];
+const first = ages[0];
+const last = ages[2];
 ages.splice(4, 1);
-ages.splice(5, 1, 'ABOBA')
-ages.push('BIBA')
-ages.shift()
-ages.unshift('Start')
-console.log({first, last});
+ages.splice(5, 1, 'ABOBA');
+ages.push('BIBA');
+ages.shift();
+ages.unshift('Start');
+console.log({ first, last });
 console.log(ages)
