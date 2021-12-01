f733a6a26b9a2e3e8280a1d75824b4151655bc7b
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-12-01 09:23:07 +0200

    Create 8-map.py
    Translated 8-map.js from js to python

---
+++ lecture_notes/08 collections/8-map.py
@@ -0,0 +1,10 @@
+ages = {"Vasia Pupkin": 19, "Marcus Aurelius": 1860}
+print({'ages': ages})
+
+ages["Vasia Pupkin"] = 20
+print({'ages': ages})
+
+ages.pop("Vasia Pupkin")
+print({'ages': ages})
+
+print('Vasia Pupkin:', 'Vasia Pupkin' in ages, "  " 'Marcus Aurelius:', 'Marcus Aurelius' in ages)
