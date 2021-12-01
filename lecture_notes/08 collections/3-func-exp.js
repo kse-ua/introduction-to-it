7566e1e4a0a754fa5efd4e30e2005c4a8de05c70
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:16:14 +0200

    Compare 3-func-exp.js and 3-lambda.py
    Put comments, where lines are different

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -2,13 +2,13 @@
 
 // Implementation
 
-const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
+const getFirstAndLast = (array) => ({ //In python we don't declare a function, we use lambda instead
+    first: array[0], //This line is the same in python
+    last: array[array.length - 1], // Here we use length to find the last element, in python we [number of last]
 });
 
 // Usage
 
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; // This line is the same in python
+const { first, last } = getFirstAndLast(ages); // This line is the same in python
+console.log({ first, last }); // We use print function in python in comparison to 'console.log' in Js
