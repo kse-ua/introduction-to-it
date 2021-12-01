1ee28228b7620890249fa0a00be0560f7dc24052
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-01 09:25:46 +0200

    still trying to write a fuction

--- lecture_notes/08 collections/2-function.cpp
+++ lecture_notes/08 collections/2-function.cpp
@@ -1,6 +1,9 @@
-int get_first_and_last(int array[]) {
+#include <iostream>
+using namespace std;
+
+int get_first_and_last(int array[9]) {
   int first = array[0];
-  int length = sizeof(array[]) / sizeof(array[0]);
+  int length = sizeof(array[9]) / sizeof(int);
   int last = array[length - 1];
   
   printf("first: %d\n", first);
@@ -9,7 +12,8 @@ int get_first_and_last(int array[]) {
 
 int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
 
+
 int main() {
-  int result = get_first_and_last(ages[])
-  printf("result: %d\n", result)
+  int result = get_first_and_last(ages[9])
+  printf("result: %d\n", result);
 }
