53604d10b96d9ffc179c3c9b5e5f42b9abb3ebbb
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-01 11:04:38 +0200

    Tasks for lecture 8

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -6,3 +6,17 @@ const first = ages[0];
 const last = ages[ages.length - 1];
 
 console.log({ first, last });
+
+// Remove elements from the end
+ages.pop();
+
+// Remove elements from the beginning
+ages.shift();
+
+// Add elements to the beginning
+ages.unshift(4, 13);
+
+// Add elements to the end
+ages.push(25, 31)
+
+console.log("The new list is:", ages)
--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -3,4 +3,18 @@ ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first = ages[0]
 last = ages[-1]
 
-print({ 'first': first, 'last': last })
+print({"first": first, "last": last})
+
+# Remove using pop(index) method
+ages.pop(0)
+
+# Remove using remove(value) method
+ages.remove(20)
+
+# Add to the beginning
+ages.insert(0, 4)
+
+# Add to the end
+ages.append(25)
+
+print("The new list is:", ages)
