a88dc2a2fb77ea500aec333553a4526446ad0cb4
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-01 11:17:20 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -8,8 +8,7 @@ const getFirstAndLast = (array) => ({
 });
 
 const concat = (arr1, arr2) => {
-  const arr = arr1.slice();
-  arr.push(...arr2);
+  const arr = arr1.concat(arr2)
   return arr;
 };
 
