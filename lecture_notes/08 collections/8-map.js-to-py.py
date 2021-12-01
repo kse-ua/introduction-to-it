67644839d3acaa92d3dc112852ea92e8f29ff3f8
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-01 10:18:11 +0200

    Update and rename 8-map.js to 8-map.js-to-py.py

---
+++ lecture_notes/08 collections/8-map.js-to-py.py
@@ -0,0 +1,16 @@
+dict = ['Vasia Pupkin', 'Marcus Aurelius']
+
+Names_IQ = {
+    'Vasia Pupkin': 19,
+    'Marcus Aurelius': 1860,
+}
+print(Names_IQ)
+
+del Names_IQ['Vasia Pupkin']
+print(Names_IQ)
+
+print('Vasia Pupckin' in dict,'Marcus Aurelius' in dict)
+
+dict.clear()
+print(dict)
+
