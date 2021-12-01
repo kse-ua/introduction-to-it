58e8757782c8bc54eeddf0d3d0ba939b562e5ffc
Kate732 <kmaistrenko@kse.org.ua>
2021-12-01 10:36:55 +0200

    6-set-changed

---
+++ lecture_notes/08 collections/6-set.py
@@ -0,0 +1,10 @@
+set_first = {10, 12, 15, 15, 17, 18, 18, 19, 20}
+
+set_first.add('7')
+set_first.add(7)
+set_first.remove(12)
+
+print(15 in set_first)
+print(7 in set_first)
+print(12 in set_first)
+print(20 in set_first)
