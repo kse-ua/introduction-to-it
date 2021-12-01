4e0e4fe5aad9156f468547039151fe188b1cb5cb
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:10:07 +0200

    Modified 1-list Python
    Add/remove at the end of array (append; pop)
    Add/remove at the top of array (insert, pop)

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -4,3 +4,15 @@ first = ages[0]
 last = ages[-1]
 
 print({ 'first': first, 'last': last })
+
+ages.append(21) # Add at the end of list
+print(ages)
+
+ages.pop()
+print(ages) # Remove  at the end of list
+
+ages.insert(0,9) # Add at the top of list
+print(ages)
+
+ages.pop(0)
+print(ages) # Remove at the top of list
