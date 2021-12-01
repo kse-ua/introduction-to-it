5c26327f184e4127890f5cabddf6c5bb84c413d4
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-12-01 10:59:55 +0200

    Translated b-union.js to py

---
+++ lecture_notes/08 collections/b-union.py
@@ -0,0 +1,9 @@
+intersection = lambda s1, s2: s1 & s2
+
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
+
+print({'cities1': cities1, 'cities2': cities2})
+
+results = intersection(cities1, cities2)
+print(results)
