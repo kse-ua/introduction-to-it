69e5dbf1d097fe0f5e308035fd9b7b9ff972ff4f
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:17:21 +0200

    Compared 3-func-exp.js and 3-lambda.py
    Put comments, where lines are different

--- lecture_notes/08 collections/3-lambda.py
+++ lecture_notes/08 collections/3-lambda.py
@@ -1,9 +1,9 @@
 # Implementation
 
-getFirstAndLast = lambda array: (array[0], array[-1])
+getFirstAndLast = lambda array: (array[0], array[-1]) # To declare function in JS we use const name  = () =>
 
 # Usage
 
-ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-first, last = getFirstAndLast(ages)
-print({ 'first': first, 'last': last })
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20] # This line is the same in JS
+first, last = getFirstAndLast(ages) # This line is the same in JS
+print({ 'first': first, 'last': last }) # To print elements in JS we use 'console.log'
