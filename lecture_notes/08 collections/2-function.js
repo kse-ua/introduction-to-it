41ef046d516a260defaf53536afacca13f3cc892
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 17:17:24 +0200

    Update 2-function JavaScript

--- lecture_notes/08 collections/2-function.js
+++ lecture_notes/08 collections/2-function.js
@@ -1,14 +1,21 @@
 
 // Implementation
 
-const getFirstAndLast = (array) => {  //To declare function in python we use def
-    const first = array[0]; // This line is the same in python
-    const last = array[array.length - 1]; // Here we use length to find the last element, in python we use [number of last]
-    return { first, last }; // This line is the same in python
+//To declare function in python we use def
+const getFirstAndLast = (array) => {
+    // This line is the same in python
+    const first = array[0]; 
+    // Here we use length to find the last element, in python we use [number]
+    const last = array[array.length - 1]; 
+    // This line is the same in python
+    return { first, last }; 
 };
 
 // Usage
 
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; // This line is the same in python
-const { first, last } = getFirstAndLast(ages); // This line is the same in python
-console.log({ first, last });  // We use print function in python in comparison to console.log in Js
+// This line is the same in python
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; 
+// This line is the same in python
+const { first, last } = getFirstAndLast(ages); 
+// We use print function in python in comparison to console.log in Js
+console.log({ first, last });  
