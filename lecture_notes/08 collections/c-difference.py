d691c1a2bc025234efa620309f05e79655a65a0a
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-12-01 11:06:34 +0200

    Translated c-difference.js to python

---
+++ lecture_notes/08 collections/c-difference.py
@@ -0,0 +1,9 @@
+difference = lambda s1, s2: s1.difference(s2)
+
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
+
+print({'cities1': cities1, 'cities2': cities2})
+
+results = difference(cities1, cities2)
+print(results)
