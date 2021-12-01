20edf6563bb60b1f222e58d97aeb4340642c41f2
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-01 09:13:34 +0200

    Update 2-function.js

--- lecture_notes/08 collections/2-function.js
+++ lecture_notes/08 collections/2-function.js
@@ -13,3 +13,7 @@ const getFirstAndLast = (array) => {
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const { first, last } = getFirstAndLast(ages);
 console.log({ first, last });
+
+// JS code is pretty the same, however in JS code we can not set "-1" index, as we do in python, thus, we subtract 1 from
+// the len of an array. Of course, to print smth in JS we should use .log and semicolon after declaring a var.
+// Generally, JS has almost thr same syntax as PY.
