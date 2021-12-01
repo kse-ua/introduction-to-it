d8d417ebbcffc97499324b4a05ac00edcbe059f0
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-01 12:15:51 +0200

    still trying

--- lecture_notes/08 collections/2-function.cpp
+++ lecture_notes/08 collections/2-function.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 using namespace std;
+#include <tuple>
 
-int get_first_and_last(int array[9]) {
+int get_first_and_last(int array[], int length) {
   int first = array[0];
-  int length = sizeof(array[9]) / sizeof(int);
   int last = array[length - 1];
-  
-  printf("first: %d\n", first);
-  printf("last: %d\n", last);
+
+  tuple <int> result (first, last);
+  return result;
 }
 
-int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
-
-
 int main() {
-  int result = get_first_and_last(ages[9])
-  printf("result: %d\n", result);
+  int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
+  int length = sizeof(ages[]) / sizeof(int);
+  int finalResult = get_first_and_last(ages[9], length);
+  printf("result: %d\n", finalResult);
 }
