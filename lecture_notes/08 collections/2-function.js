97c77d1bd129d3dc33deea971bf66a7b0fc25399
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:11:16 +0200

    Compare JavaScript and Python examples
    Comments, where lines are different

--- lecture_notes/08 collections/2-function.js
+++ lecture_notes/08 collections/2-function.js
@@ -1,15 +1,14 @@
-'use strict';
 
 // Implementation
 
-const getFirstAndLast = (array) => {
-  const first = array[0];
-  const last = array[array.length - 1];
-  return { first, last };
+const getFirstAndLast = (array) => {  //To declare function in python we use def
+    const first = array[0]; // This line is the same in python
+    const last = array[array.length - 1]; // Here we use length to find the last element, in python we use [number of last]
+    return { first, last }; // This line is the same in python
 };
 
 // Usage
 
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; // This line is the same in python
+const { first, last } = getFirstAndLast(ages); // This line is the same in python
+console.log({ first, last });  // We use print function in python in comparison to console.log in Js
