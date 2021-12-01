5aa7051fa7375f646878f71fc73bfcf158fdb714
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-01 11:29:35 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -1,8 +1,8 @@
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
-const wholeSet = schoolAges.concat(studentAges)
-first = c[0]
-last = c[c.length - 1]
+const wholeSet = schoolAges.concat(studentAges);
+first = c[0];
+last = c[c.length - 1];
 
 
 console.log({first, last})
