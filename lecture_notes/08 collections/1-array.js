a07c578008c70bd5ae18a3b1994d3e6286f8bd02
Kate732 <kmaistrenko@kse.org.ua>
2021-12-01 10:15:38 +0200

    1-arrays-changed

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -5,4 +5,13 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const first = ages[0];
 const last = ages[ages.length - 1];
 
+const addAtEnd = ages.push(21)
+console.log(addAtEnd);
+const deleteFromEnd = ages.pop()
+console.log(deleteFromEnd);
+const addAtTop = ages.unshift(9)
+console.log(addAtTop);
+const deleteFromTop = ages.shift()
+console.log(deleteFromTop);
+
 console.log({ first, last });
--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -3,4 +3,13 @@ ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first = ages[0]
 last = ages[-1]
 
+ages.append(21)
+print(ages)
+ages.pop()
+print(ages)
+ages.insert(0, 9)
+print(ages)
+ages.remove(9)
+print(ages)
+
 print({ 'first': first, 'last': last })
