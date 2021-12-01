2229058d606697bd5f8b75d4fde3ec2aee60b05f
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-01 04:06:26 +0200

    Update and rename 6-set.js to 6-set-editet-for-py.py

---
+++ lecture_notes/08 collections/6-set-editet-for-py.py
@@ -0,0 +1,16 @@
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
+print(ages)
+
+ages.append(16);
+ages.remove(20);
+
+def printAgeIfInList(*x):
+    print(x)
+    for i in range(len(x)-1):
+        if x[i+1] in x[0]:
+            print(x[i+1],": True")
+        else:
+            print(x[i+1],": False")
+printAgeIfInList(ages,10,16,19,20)
+ages.clear()
+print(ages)
