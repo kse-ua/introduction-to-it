06f1c202640a8ac907b62a4e57175fea6b7dc57c
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-01 10:14:57 +0200

    2-function C++
    Tried to implement it with C++

---
+++ lecture_notes/08 collections/2-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int printFirstLast(const int ages[], int length)
+{
+    int first = ages[0];
+    int last = ages[length - 1];
+    cout << "First element: " << first << endl;
+    cout << "Last element: " << last << endl;
+}
+
+int main()
+{
+    int ages[] = {10, 12, 15, 15, 17, 18, 18, 19, 20};
+    int length = sizeof(ages) / sizeof(ages[0]);
+    printFirstLast(ages, length);
+    return 0;
+}
