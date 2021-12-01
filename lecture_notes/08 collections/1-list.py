e6d0bb707e4702c141f2bd8dba72ea5237a4f486
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-01 10:51:08 +0200

    Update 1-list.py

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,5 +1,17 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
+# del last elem fron the list
+ages.pop()
+
+# add elem at the beggining of the list
+ages.insert(0, 9)
+
+# del first elem
+del ages[0]
+
+# add elem at the end
+ages.append(21)
+
 first = ages[0]
 last = ages[-1]
 
