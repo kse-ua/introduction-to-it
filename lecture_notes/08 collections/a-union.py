c1740b9de28c330c35658ff362004f50b3869b74
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-01 17:45:42 +0200

    Update a-union.py

--- lecture_notes/08 collections/a-union.py
+++ lecture_notes/08 collections/a-union.py
@@ -1,10 +1,10 @@
+def union_cities(s1, s2):
+    difference = set(s2) - set(s1)
+    result = s1 + list(difference)
+    return result
+
+
 cities1 = ['Beijing', 'Kiev']
 cities2 = ['Kiev', 'London', 'Baghdad']
 
-first_cities = set(cities1)
-second_cities = set(cities2)
-
-dif = second_cities - first_cities
-
-result = cities1 + list(dif)
-print(result)
+print(union_cities(cities1, cities2))
