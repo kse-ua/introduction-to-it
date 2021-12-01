4230eb1aeefe26995a0d3fcb13fc29d094f6ab60
Uliana Makhno <94102483+UlianaMakhno@users.noreply.github.com>
2021-12-01 11:37:22 +0200

    Create 1-array.js

---
+++ 1-array.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+
+const first = ages[0];
+const last = ages[ages.length - 1];
+
+console.log({ first, last });
+
+ages.pop();
+
+console.log(ages);
+
+ages.push('20');
+
+console.log(ages);
+
+ages.shift();
+
+console.log(ages);
+
+ages.unshift('10');
