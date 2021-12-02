6f32d0a100fd946aa3dd6f89335be4f4b755a1e8
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-02 17:55:22 +0200

    Fixed Code style

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -1,15 +1,11 @@
 'use strict';
-// Implementation
-
-const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
-});
-
-// Usage
 
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = (schoolAges.concat(studentAges));
+const getFirstAndLast = (array) => ({
+    first: array[0],
+    last: array[array.length - 1],
+});
 const result = getFirstAndLast(ages);
 console.log(result);
