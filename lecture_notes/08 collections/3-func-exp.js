7d03ff0ebba166c823bb0a848a4c2a1938948e33
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 23:38:54 +0300

    Update 3-func-exp.js

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -1,14 +1,19 @@
 'use strict';
 
 // Implementation
-
+// in python use def
 const getFirstAndLast = (array) => ({
+  // the same
   first: array[0],
+  // in python use [number]
+  // the same
   last: array[array.length - 1],
 });
 
 // Usage
-
+// the same
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+// the same
 const { first, last } = getFirstAndLast(ages);
+// print function in python
 console.log({ first, last });
