2294f8ea61a5ab368d0e484e0bd1a6460498eb49
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-02 18:39:52 +0200

    Added semicolon

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -18,6 +18,6 @@ const concat = (arr1, arr2) => {
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
-console.log(ages)
+console.log(ages);
 const { first, last } = getFirstAndLast(ages);
 console.log({ first, last });
