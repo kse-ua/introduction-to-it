11e9e9e096f206cceae51f6f9b649fdc04137a3b
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 23:01:26 +0300

    Update 2-function.py
    Compare JavaScript and Python examples

--- lecture_notes/08 collections/2-function.py
+++ lecture_notes/08 collections/2-function.py
@@ -1,12 +1,12 @@
 # Implementation
 
-def getFirstAndLast(array):
-  first = ages[0]
-  last = ages[-1]
-  return first, last
+def getFirstAndLast(array): # in JS we use const name  = () =>
+  first = ages[0] # the same
+  last = ages[-1] # in JS use 'length-1'
+  return first, last # the same 
 
 # Usage
 
-ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-first, last = getFirstAndLast(ages)
-print({ 'first': first, 'last': last })
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20] # the same
+first, last = getFirstAndLast(ages) # the same
+print({ 'first': first, 'last': last }) # in JS  use 'console.log'
