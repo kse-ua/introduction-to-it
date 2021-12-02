a5c679c6d0af78a6a34a65964843208c9063feed
bugarenko <tbuharenko@kse.org.ua>
2021-12-02 14:43:51 +0200

    modified.py

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,6 +1,10 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
+ages.remove(20)
+ages.remove(10)
+ages.pop(3)
+
 first = ages[0]
 last = ages[-1]
 
-print({ 'first': first, 'last': last })
+print(ages)
