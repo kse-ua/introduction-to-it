709d106600c24a026c6061c81de5abc13c8e9dd3
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 21:42:27 +0300

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -6,3 +6,12 @@ const first = ages[0];
 const last = ages[ages.length - 1];
 
 console.log({ first, last });
+
+ages.push(21); // # Add at the end of array
+console.log(ages);
+ages.pop(); // remove at the end of array
+console.log(ages);
+ages.shift(); // Add at the top of array
+console.log(ages);
+ages.unshift(11); // remove at the top of array
+console.log(ages);
