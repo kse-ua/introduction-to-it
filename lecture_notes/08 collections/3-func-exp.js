45eb6cd48b28d7e25bcbb440c23bf4d6fcac86fe
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-03 00:05:55 +0300

    Update 3-func-exp.js
    Compare 3-func-exp.js and 3-lambda.py

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -1,15 +1,18 @@
 'use strict';
+// Implementation
 
+//use lambda instead
 const getFirstAndLast = (array) => ({
   
-  first: array[0],
-  
+  first: array[0],   // the same 
+  // in python use [number of last]  
   last: array[array.length - 1],
 });
 
-
+// Usage
+// the same
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-
+// the same
 const { first, last } = getFirstAndLast(ages);
-
+// in python 'console.log'
 console.log({ first, last });
