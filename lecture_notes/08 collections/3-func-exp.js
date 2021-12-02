f9bcf54ddf4164ce9430a84cff9a9927c3652e26
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-02 17:37:59 +0200

    Fixed trailing spaces 3-function js

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -3,7 +3,7 @@
 // Implementation
 
 //In python we don't declare a function, we use lambda instead
-const getFirstAndLast = (array) => ({ 
+const getFirstAndLast = (array) => ({
   first: array[0], //This line is the same in python
   // Here we use length to find the last element, in python we [number of last]
   last: array[array.length - 1],
@@ -12,8 +12,8 @@ const getFirstAndLast = (array) => ({
 // Usage
 
 // This line is the same in python
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; 
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 // This line is the same in python
-const { first, last } = getFirstAndLast(ages); 
+const { first, last } = getFirstAndLast(ages);
 // We use print function in python in comparison to 'console.log'
-console.log({ first, last }); 
+console.log({ first, last });
