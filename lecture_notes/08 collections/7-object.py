bb7c568bcb47c1131a1fcf52c6af14213aa27c63
Mykhailo Sherin <94047443+msherinn@users.noreply.github.com>
2021-12-02 22:04:52 +0200

    translated 7-object.js to 7-object.py

---
+++ lecture_notes/08 collections/7-object.py
@@ -0,0 +1,13 @@
+ages = {
+  'Vasia Pupkin': 19,
+  'Marcus Aurelius': 1860,
+}
+print(ages)
+
+ages['Vasia Pupkin'] = 20
+print(ages)
+
+ages.pop('Vasia Pupkin', None)
+print(ages)
+
+print({'Vasia Pupkin': 'Vasia Pupkin' in ages, 'Marcus Aurelius': 'Marcus Aurelius' in ages})
