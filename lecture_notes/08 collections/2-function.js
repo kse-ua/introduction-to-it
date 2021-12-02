cde85bd5b9beb58295f05a8b6993f7d24b5383e2
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 23:51:35 +0300

    Update 2-function.js
    Compare JavaScript and Python examples

--- lecture_notes/08 collections/2-function.js
+++ lecture_notes/08 collections/2-function.js
@@ -2,14 +2,20 @@
 
 // Implementation
 
+// in python use def
 const getFirstAndLast = (array) => {
+  // the same
   const first = array[0];
+  // in python use [number]
   const last = array[array.length - 1];
+  // the same
   return { first, last };
 };
 
 // Usage
-
+// the same
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+// the same
 const { first, last } = getFirstAndLast(ages);
+// print function in python
 console.log({ first, last });
