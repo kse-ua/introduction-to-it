b26fa90e58a1ffbfefdd8bf1599d38c7911838b9
istepmother <imachukha@kse.org.ua>
2021-12-03 01:35:48 +0200

    fixed identation

--- lecture_notes/08 collections/9-distinct.js
+++ lecture_notes/08 collections/9-distinct.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const distinct = (dataset) => {
-  const keys = new Set();
-  return dataset.filter((record) => {
-    const cols = Object.keys(record).sort();
-    const key = cols.map((field) => record[field]).join('\x00');
-    const has = keys.has(key);
-    if (!has) keys.add(key);
-    return !has;
-  });
+    const keys = new Set();
+    return dataset.filter((record) => {
+        const cols = Object.keys(record).sort();
+        const key = cols.map((field) => record[field]).join('\x00');
+        const has = keys.has(key);
+        if (!has) keys.add(key);
+        return !has;
+    });
 };
 
 // Usage
 
 const flights = [
-  { from: 'Kiev', to: 'Rome' },
-  { from: 'Kiev', to: 'Warsaw' },
-  { from: 'Dublin', to: 'Riga' },
-  { from: 'Riga', to: 'Dublin' },
-  { from: 'Kiev', to: 'Rome' },
-  { from: 'Cairo', to: 'Paris' },
+    { from: 'Kiev', to: 'Rome' },
+    { from: 'Kiev', to: 'Warsaw' },
+    { from: 'Dublin', to: 'Riga' },
+    { from: 'Riga', to: 'Dublin' },
+    { from: 'Kiev', to: 'Rome' },
+    { from: 'Cairo', to: 'Paris' },
 ];
 
 console.dir({ flights });
