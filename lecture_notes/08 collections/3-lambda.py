9c6535fc2f2ce354c93d1a942369df1e19768428
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 23:56:24 +0300

    Update 3-lambda.py
    Compare 3-func-exp.js and 3-lambda.py

--- lecture_notes/08 collections/3-lambda.py
+++ lecture_notes/08 collections/3-lambda.py
@@ -1,9 +1,10 @@
 # Implementation
 
-getFirstAndLast = lambda array: (array[0], array[-1])
+getFirstAndLast = lambda array: (array[0], array[-1]) # in JS use const name  = () =>
+
 
 # Usage
 
-ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-first, last = getFirstAndLast(ages)
-print({ 'first': first, 'last': last })
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20] # the same
+first, last = getFirstAndLast(ages) # the same
+print({ 'first': first, 'last': last }) # use 'console.log'
