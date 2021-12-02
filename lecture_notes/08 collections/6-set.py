53d709717ee88d284271225e5361aa319b72f03b
istepmother <imachukha@kse.org.ua>
2021-12-03 01:30:58 +0200

    translated set example

---
+++ lecture_notes/08 collections/6-set.py
@@ -0,0 +1,15 @@
+ages = set([10, 12, 15, 15, 17, 18, 18, 19, 20])
+print(ages)
+
+ages.add(16)
+ages.remove(20)
+
+print({
+    10: (10 in ages),
+    16: (16 in ages),
+    19: (19 in ages),
+    20: (20 in ages),
+})
+
+ages.clear()
+print(ages)
