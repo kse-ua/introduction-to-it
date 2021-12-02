20709d39391e09423f1481675b456740fb124f83
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-02 21:25:43 +0300

    Update 1-list.py

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -6,7 +6,13 @@ last = ages[-1]
 print({ 'first': first, 'last': last })
 
 ages.append(29) # Add at the end of array
+print(ages)
+
 ages.pop() # remove at the end of array
+print(ages)
+
 ages.insert(0,5) # Add at the top of array
+print(ages)
+
 ages.pop(-1) # remove at the top of array
 print(ages)
