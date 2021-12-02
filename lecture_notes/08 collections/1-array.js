ba31ab41f5a722b73f9aebfdca8cce2f0e4af0c0
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-02 22:06:48 +0200

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -5,6 +5,7 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 new_element1, new_element2 = 5, 10;
 ages.pop();
 ages.push(new_element, new_element2);
+ages.unshift();
 ages.shift();
 
 const first = ages[0];
