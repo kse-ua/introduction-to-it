47ced081d0a300320c9aee7d329aae917b41982c
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-02 18:32:36 +0200

    pop, push and shift used

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -2,6 +2,11 @@
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 
+new_element1, new_element2 = 5, 10;
+ages.pop();
+ages.push(new_element, new_element2);
+ages.shift();
+
 const first = ages[0];
 const last = ages[ages.length - 1];
 
