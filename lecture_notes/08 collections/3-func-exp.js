a95ddc460c1426f5d414bb40d42a6ed7cf9a267c
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-02 17:22:14 +0200

    Update 3-func-exp.js

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -2,13 +2,18 @@
 
 // Implementation
 
-const getFirstAndLast = (array) => ({ //In python we don't declare a function, we use lambda instead
-    first: array[0], //This line is the same in python
-    last: array[array.length - 1], // Here we use length to find the last element, in python we [number of last]
+//In python we don't declare a function, we use lambda instead
+const getFirstAndLast = (array) => ({ 
+  first: array[0], //This line is the same in python
+  // Here we use length to find the last element, in python we [number of last]
+  last: array[array.length - 1], /
 });
 
 // Usage
 
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; // This line is the same in python
-const { first, last } = getFirstAndLast(ages); // This line is the same in python
-console.log({ first, last }); // We use print function in python in comparison to 'console.log' in Js
+// This line is the same in python
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]; 
+// This line is the same in python
+const { first, last } = getFirstAndLast(ages); 
+// We use print function in python in comparison to 'console.log'
+console.log({ first, last }); 
