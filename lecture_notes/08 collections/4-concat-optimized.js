a1d7b562a8ee396c5866ccad86dcfb8629a0e3b0
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-03 09:11:49 +0200

    4-concat-optimized.js: created function

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// Implementation
+
+const concat = (arr1, arr2) => (arr1.concat(arr2));
+
+// Usage
+
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
-const ages = schoolAges.concat(studentAges);
+const ages = concat(schoolAges, studentAges);
 console.log({ ages });
