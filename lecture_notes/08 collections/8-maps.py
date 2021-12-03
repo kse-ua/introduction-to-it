c527e55be168060d826281de56c4e3ffe41b2cfb
Iryna Sukhoiarska <94047966+ISukhoiarska@users.noreply.github.com>
2021-12-03 11:04:18 +0200

    Create 8-maps.py

---
+++ lecture_notes/08 collections/8-maps.py
@@ -0,0 +1,14 @@
+ages = {"Vasia Pupkin": 19, "Marcus Aurelius": 1860,}
+
+print(f"ages:{ages}")
+
+ages['Vasia Pupkin'] = 20
+print(f"ages:{ages}")
+
+ages.pop('Vasia Pupkin')
+print(f"ages:{ages}")
+
+print(f"Vasia Pupkin: {'Vasia Pupkin' in ages}, Marcus Aurelius: {'Marcus Aurelius' in ages}")
+
+ages = {}
+print({'ages': ages})
