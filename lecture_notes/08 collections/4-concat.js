a8faa4b4ff7ac5bb5536b1f15ca64428617cbe26
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-03 10:06:24 +0200

    Exaple CONTACT

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -6,3 +6,4 @@ const UpperCase1 = ArrLowerCase1.map(name => name.toUpperCase());
 const UpperCase2 = ArrLowerCase2.map(name => name.toUpperCase());
 
 console.log(UpperCase1.concat(UpperCase2)); // ["H", "E", "L", "L", "O", "W", "O", "R", "L", "D"]
+
