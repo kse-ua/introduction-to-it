fbe67dceb4ce834094a4f8608ec99655aee98228
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 13:11:09 +0200

    Update 4-concat-with-func.cpp

--- lecture_notes/08 collections/4-concat-with-func.cpp
+++ lecture_notes/08 collections/4-concat-with-func.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 
-void sumArray(int m,int n,int array1[],int array2[]){
-    int arr[m + n];
-    for (int i = 0; i < m + n; i++)
-    {
-        if (i < m) {
-            arr[i] = array1[i];
-        }
-        else {
-            arr[i] = array2[i - m];
-        }
+void sumArray (int m, int n, int array1[], int array2[]) {
+  int arr[m + n];
+  for (int i = 0; i < m + n; i++) {
+    if (i < m) {
+      arr[i] = array1[i];
     }
-    for (int i = 0; i < m+n; i++) {
-    printf("ages[%d]: %d\n", i, arr[i]);
+    else {
+      arr[i] = array2[i - m];
+    }
   }
+  for (int i = 0; i < m + n; i++) {
+  printf("ages[%d]: %d\n", i, arr[i]);
+    }
 }
 int main() {
   int schoolAges[] = { 10, 12, 15, 15 };
   int studentAges[] = { 17, 18, 18, 19, 20 };
   int schoolLength = sizeof(schoolAges) / sizeof(int);
   int studentLength = sizeof(studentAges) / sizeof(int);
-  sumArray(schoolLength,studentLength,schoolAges,studentAges);
+  sumArray(schoolLength, studentLength, schoolAges, studentAges);
 
 }
