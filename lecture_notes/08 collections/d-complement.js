08bf5c2d217e0f49a67714c85cf2ee39afb167df
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-03 09:25:03 +0200

    translate-complement-into-python

--- lecture_notes/08 collections/d-complement.js
+++ lecture_notes/08 collections/d-complement.js
@@ -1,25 +1,8 @@
-'use strict';
+difference = lambda a, b: a - b
+complement = lambda a, b: difference(a, b)
 
-const difference = (s1, s2) => {
-  const ds = [];
-  for (let i = 0; i < s1.length; i++) {
-    const item = s1[i];
-    if (!s2.includes(item)) ds.push(item);
-  }
-  return ds;
-};
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
 
-// const difference = (s1, s2) => new Set(
-//   [...s1].filter((v) => !s2.has(v))
-// );
-
-const complement = (s1, s2) => difference(s2, s1);
-
-// Usage
-
-const cities1 = ['Beijing', 'Kiev'];
-const cities2 = ['Kiev', 'London', 'Baghdad'];
-console.dir({ cities1, cities2 });
-
-const results = complement(cities1, cities2);
-console.dir(results);
+result = complement(cities2, cities1)
+print(result)
