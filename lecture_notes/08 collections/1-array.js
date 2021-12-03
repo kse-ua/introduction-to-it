ef4e6879d786d256506feced742dd5a406ce43b9
macbook <macbook@MacBookAir-Anastasia.local>
2021-12-03 14:17:39 +0200

    Working with list

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -2,7 +2,14 @@
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 
+ages.push(21)
+
+ages.pop()
+
+ages.shift()
+ages.unshift(10)
+
+
 const first = ages[0];
 const last = ages[ages.length - 1];
-
 console.log({ first, last });
--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -2,5 +2,9 @@ ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
 first = ages[0]
 last = ages[-1]
+ages.pop()
+ages.insert(0, 9)
+del ages[0]
+ages.append(21)
 
-print({ 'first': first, 'last': last })
+print({'first': first, 'last': last})
