ee340ee190642fad17f97b77f456d124a05bb37f
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 13:01:46 +0200

    Update 1-array-modyfied.js

--- lecture_notes/08 collections/1-array-modyfied.js
+++ lecture_notes/08 collections/1-array-modyfied.js
@@ -5,7 +5,8 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const first = ages[0];
 const last = ages[ages.length - 1];
 const arrayForNewElements = ages.reverse();
-arrayForNewElements.filter((element) => element > 16 || element === 12);
+const newFilterdArray = arrayForNewElements.filter((element) => element > 16 || element === 12);
+
 const newSlicedArray = arrayForNewElements.slice(1, 5);
 const newEmptyArray = new Array(5);
 newEmptyArray.fill('bang');
@@ -13,7 +14,7 @@ const arrayWithGun = newEmptyArray.map((element) => 'bim' + element);
 
 // result:
 console.log({ first, last });
-console.log(arrayForNewElements, 'filtered array');
+console.log(newFilterdArray, 'filtered array');
 console.log(newSlicedArray, 'sliced array');
 console.log(newEmptyArray.join('-'), 'new empty array filed with "bang"');
 console.log(arrayWithGun.join('-'));
