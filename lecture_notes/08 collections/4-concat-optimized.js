d91468484894ed94d14e91595138544039088f58
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-03 09:22:40 +0200

    Removed spaces

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -1,11 +1,9 @@
 'use strict';
 
 // Implementation
-
 const concat = (arr1, arr2) => (arr1.concat(arr2));
 
 // Usage
-
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
