b5bac07bd2c54cc722739dd86cbb0f554ca180e3
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-03 09:38:58 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -2,9 +2,9 @@
 
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
-const wholeSet = schoolAges.concat(studentAges)
-const first = NewSet[0];
-const last = NewSet[NewSet.length - 1];
 
+const wholeSet = schoolAges.concat(studentAges);
+const first = array[0];
+const last = array[array.length - 1];
 
-console.log({first, last})
+console.log({first, last});
