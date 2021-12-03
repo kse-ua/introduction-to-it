556de3dc46d749673ac98d356249ac86417032c4
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-03 11:24:24 +0200

    translate-9-distinct- to-py

--- lecture_notes/08 collections/9-distinct.js
+++ lecture_notes/08 collections/9-distinct.js
@@ -1,29 +1,18 @@
-'use strict';
+def distinct(dataset):
+    directions = []
+    for direction in dataset:
+        if direction not in directions:
+            directions.append(direction)
+    print(f"directions:\n {directions}")
 
-const distinct = (dataset) => {
-  const keys = new Set();
-  return dataset.filter((record) => {
-    const cols = Object.keys(record).sort();
-    const key = cols.map((field) => record[field]).join('\x00');
-    const has = keys.has(key);
-    if (!has) keys.add(key);
-    return !has;
-  });
-};
 
-// Usage
+flights = [
+  {'from': 'Kiev', 'to': 'Rome'},
+  {'from': 'Kiev', 'to': 'Warsaw'},
+  {'from': 'Dublin', 'to': 'Riga'},
+  {'from': 'Riga', 'to': 'Dublin'},
+  {'from': 'Kiev', 'to': 'Rome'},
+  {'from': 'Cairo', 'to': 'Paris'},
+]
 
-const flights = [
-  { from: 'Kiev', to: 'Rome' },
-  { from: 'Kiev', to: 'Warsaw' },
-  { from: 'Dublin', to: 'Riga' },
-  { from: 'Riga', to: 'Dublin' },
-  { from: 'Kiev', to: 'Rome' },
-  { from: 'Cairo', to: 'Paris' },
-];
-
-console.dir({ flights });
-
-const directions = distinct(flights);
-
-console.dir({ directions });
+distinct(flights)
