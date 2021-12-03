194fb92c6cd5fe6666503df65533d3c2b9e8041f
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-03 13:00:55 +0200

    Update 4-concat.cpp

--- lecture_notes/08 collections/4-concat.cpp
+++ lecture_notes/08 collections/4-concat.cpp
@@ -1,23 +1,25 @@
-#include <stdio.h>
+using namespace std;
+#include <iostream>
+
+int* concat_array(int array1[], int array2[], int firstLength, int secondLength){
+    int length = firstLength + secondLength;
+    int new_array[length];
+
+    for(int i = 0; i < firstLength; i++){
+        new_array[i] = array1[i];
+    }
+    for(int i = 0; i < secondLength; i++){
+        new_array[i + firstLength] = array2[i];
+    }
+    for(int i = 0; i < length; i++){
+        cout << new_array[i] << endl;
+    }
+}
 
 int main() {
-  int schoolAges[] = { 10, 12, 15, 15 };
-  int studentAges[] = { 17, 18, 18, 19, 20 };
-
-  int schoolLength = sizeof(schoolAges) / sizeof(int);
-  int studentLength = sizeof(studentAges) / sizeof(int);
-  int length = schoolLength + studentLength;
-  int ages[length];
-
-  for (int i = 0; i < schoolLength; i++) {
-    ages[i] = schoolAges[i];
-  }
-
-  for (int i = 0; i < studentLength; i++) {
-    ages[i + schoolLength] = studentAges[i];
-  }
-
-  for (int i = 0; i < length; i++) {
-    printf("ages[%d]: %d\n", i, ages[i]);
-  }
+    int schoolAges[] = {10, 12, 15, 15};
+    int studentAges[] = {17, 18, 18, 19, 20};
+    int firstLength = sizeof(schoolAges) / sizeof(int);
+    int secondLength = sizeof(studentAges) / sizeof(int);
+    concat_array(schoolAges, studentAges, firstLength, secondLength);
 }
