7cdf600d1d20712800a3fcb0fba531ad6f934386
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-03 11:13:06 +0300

    Update 3-func-exp.js

--- lecture_notes/08 collections/3-func-exp.js
+++ lecture_notes/08 collections/3-func-exp.js
@@ -4,13 +4,13 @@
 
 //use lambda instead
 const getFirstAndLast = (array) => ({
-
   first: array[0],   // the same
   // in python use [number of last]
   last: array[array.length - 1],
 });
 
 // Usage
+
 // the same
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 // the same
