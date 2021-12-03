4dc95ffdb79e6eb0955df56cfce66d8f34aacf86
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 09:50:10 +0200

    Update 1-array-modyfied.js

--- lecture_notes/08 collections/1-array-modyfied.js
+++ lecture_notes/08 collections/1-array-modyfied.js
@@ -7,23 +7,23 @@ const last = ages[ages.length - 1];
 
 console.log({ first, last });
 
-const array_for_new_elements = ages.reverse();
-console.log(array_for_new_elements);
-array_for_new_elements.pop();
-array_for_new_elements.push("new last element");
-array_for_new_elements.shift();
-array_for_new_elements.unshift("new first element");
-console.log(array_for_new_elements);
-console.log(array_for_new_elements.filter(element => element > 16 || element === 12),"filtered array")
+const arrayForNewElements = ages.reverse();
+console.log(arrayForNewElements);
+arrayForNewElements.pop();
+arrayForNewElements.push("new last element");
+arrayForNewElements.shift();
+arrayForNewElements.unshift("new first element");
+console.log(arrayForNewElements);
+console.log(arrayForNewElements.filter(element => element > 16 || element === 12),"filtered array")
 
-const sliced_array = array_for_new_elements.slice(1,5);
-console.log(sliced_array);
-console.log(sliced_array.includes(18));
+const newSlicedArray = arrayForNewElements.slice(1,5);
+console.log(newSlicedArray);
+console.log(newSlicedArray.includes(18));
 
-const new_empty_array = new Array(5);
-new_empty_array.fill("bang");
-console.log(new_empty_array);
-console.log(new_empty_array.join("-"));
+const newEmptyArray = new Array(5);
+newEmptyArray.fill("bang");
+console.log(newEmptyArray);
+console.log(newEmptyArray.join("-"));
 
-const array_with_gun = new_empty_array.map(element => "bim" + element)
-console.log(array_with_gun.join(""));
+const arrayWithGun = newEmptyArray.map(element => "bim" + element)
+console.log(arrayWithGun.join(""));
