78a1ec18c8df3301758b53996131818374cda464
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-03 09:42:18 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -2,10 +2,12 @@
 
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
-const ages = (schoolAges.concat(studentAges));
+
+const ages = schoolAges.concat(studentAges);
 const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
+    first: array[0],
+    last: array[array.length - 1],
 });
+
 const result = getFirstAndLast(ages);
 console.log(result);
