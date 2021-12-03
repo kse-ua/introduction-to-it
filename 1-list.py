ae1ceec9743df5acdec9ca55900d768cd946dd2d
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 16:08:22 +0200

    Create 1-list.py

---
+++ 1-list.py
@@ -0,0 +1,24 @@
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
+
+first = ages[0]
+last = ages[-1]
+
+print(ages)
+
+ages.append(777893) 
+
+print(ages)
+
+print({ages.pop()})
+
+print(ages)
+
+ages.insert(0, 9564)
+
+print(ages)
+
+ages.pop(0)
+
+print(ages)
+
+print({'first': first, 'last': last})
