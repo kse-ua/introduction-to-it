f9237fac0fef84bb92022f3c66f8a57ae46b862a
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 10:51:02 +0200

    Update 1-array-modyfied.js

--- lecture_notes/08 collections/1-array-modyfied.js
+++ lecture_notes/08 collections/1-array-modyfied.js
@@ -1,29 +1,19 @@
 'use strict';
 
+//usage:
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-
 const first = ages[0];
 const last = ages[ages.length - 1];
-
-console.log({ first, last });
-
 const arrayForNewElements = ages.reverse();
-console.log(arrayForNewElements);
-arrayForNewElements.pop();
-arrayForNewElements.push('new last element');
-arrayForNewElements.shift();
-arrayForNewElements.unshift('new first element');
-console.log(arrayForNewElements);
 arrayForNewElements.filter((element) => element > 16 || element === 12);
-console.log(arrayForNewElements, 'filtered array');
 const newSlicedArray = arrayForNewElements.slice(1, 5);
-console.log(newSlicedArray);
-console.log(newSlicedArray.includes(18));
-
 const newEmptyArray = new Array(5);
 newEmptyArray.fill('bang');
-console.log(newEmptyArray);
-console.log(newEmptyArray.join('-'));
-
 const arrayWithGun = newEmptyArray.map((element) => 'bim' + element);
-console.log(arrayWithGun.join(''));
+
+// result:
+console.log({ first, last });
+console.log(arrayForNewElements, 'filtered array');
+console.log(newSlicedArray, 'sliced array');
+console.log(newEmptyArray.join('-'), 'new empty array filed with "bang"');
+console.log(arrayWithGun.join('-'), ': new array maped from "bang" array with more powerful weapon');
