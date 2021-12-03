8a8a5b8996ff8625d6dd005018455926d359c876
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-03 10:02:41 +0200

    d-complement.js => py

--- lecture_notes/08 collections/d-complement.js
+++ lecture_notes/08 collections/d-complement.js
@@ -1,25 +1,10 @@
-'use strict';
+difference = lambda set1, set2: set1.difference(set2)
+complement = lambda set1, set2: difference(set2, set1)
 
-const difference = (s1, s2) => {
-  const ds = [];
-  for (let i = 0; i < s1.length; i++) {
-    const item = s1[i];
-    if (!s2.includes(item)) ds.push(item);
-  }
-  return ds;
-};
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
 
-// const difference = (s1, s2) => new Set(
-//   [...s1].filter((v) => !s2.has(v))
-// );
+print({'cities1': cities1, 'cities2': cities2})
 
-const complement = (s1, s2) => difference(s2, s1);
-
-// Usage
-
-const cities1 = ['Beijing', 'Kiev'];
-const cities2 = ['Kiev', 'London', 'Baghdad'];
-console.dir({ cities1, cities2 });
-
-const results = complement(cities1, cities2);
-console.dir(results);
+results = complement(cities1, cities2)
+print(results)
