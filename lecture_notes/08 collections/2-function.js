3c238cd04c465eb2ce8f4cb58c45b30b803b6c01
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-03 16:52:52 +0200

    Update 2-function.js

--- lecture_notes/08 collections/2-function.js
+++ lecture_notes/08 collections/2-function.js
@@ -1,15 +1,21 @@
 'use strict';
-
 // Implementation
 
+// To declare function in python we use def
 const getFirstAndLast = (array) => {
+  // This line is the same in python
   const first = array[0];
+  // Here we use length to find the last element, in python we use [number]
   const last = array[array.length - 1];
+  // This line is the same in python
   return { first, last };
 };
 
 // Usage
 
+// This line is the same in python
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+// This line is the same in python
 const { first, last } = getFirstAndLast(ages);
+// We use print function in python in comparison to console.log in Js
 console.log({ first, last });
