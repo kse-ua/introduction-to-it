1fd2f5ac484fb9607c9988f4718f791614a0577c
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 16:35:30 +0200

    Create c-difference.py

---
+++ c-difference.py
@@ -0,0 +1,11 @@
+difference = lambda s1, s2: s1.difference(s2)
+
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
+
+print({f"cities1": cities1})
+print({f"cities2": cities2})
+
+results = difference(cities1, cities2)
+
+print(results)
