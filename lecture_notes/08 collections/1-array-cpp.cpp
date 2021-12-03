6b7bf3af0f7a67d46fffbda9b8ab0e8b64258c45
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 11:19:47 +0200

    Update 1-array-cpp.cpp

--- lecture_notes/08 collections/1-array-cpp.cpp
+++ lecture_notes/08 collections/1-array-cpp.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
   int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
@@ -6,8 +8,13 @@ int main() {
   int first = ages[0];
   int length = sizeof(ages) / sizeof(ages[0]);
   int last = ages[length - 1];
-
-
   printf("first: %d\n", first);
   printf("last: %d\n", last);
+  
+  ages[4] = 4;
+  printf("new element: %d\n", ages[4]);
+  printf("function elements: %d\n");
+  for(int i = 0; i < length; i++) {
+      cout << ages[i] << "\n";
+  }
 }
