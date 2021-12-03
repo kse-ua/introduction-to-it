2dfb228eabb6b44c4f3399a126ab8d4a0ee71d54
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2021-12-03 16:25:23 +0200

    add cpp

--- lecture_notes/08 collections/1-array.cpp
+++ lecture_notes/08 collections/1-array.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <vector>
 
 int main() {
-  int ages[] = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
+    std::vector <int> ages = { 10, 12, 15, 15, 17, 18, 18, 19, 20, 45 };
 
-  int first = ages[0];
-  int length = sizeof(ages) / sizeof(ages[0]);
-  int last = ages[length - 1];
+    ages.push_back(34);
 
+    int first = ages[0];
+    int length;
+    length = ages.size();
+    int last = ages[length - 1];
 
-  printf("first: %d\n", first);
-  printf("last: %d\n", last);
+    printf("first: %d\n", first);
+    printf("last: %d\n", last);
 }
+
+
