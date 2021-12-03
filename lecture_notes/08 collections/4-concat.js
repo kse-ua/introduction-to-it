7f8aaaacaa05d079da10c6d43115400c2aa2079f
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-03 09:35:28 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -1,22 +1,8 @@
-'use strict';
+// Exaple CONTACT
+const ArrLowerCase1 = ['h', 'e', 'l', 'l', 'o'];
+const ArrLowerCase2 = ['w', 'o', 'r', 'l', 'd'];
 
-// Implementation
+const UpperCase1 = ArrLowerCase1.map(name => name.toUpperCase());
+const UpperCase2 = ArrLowerCase2.map(name => name.toUpperCase());
 
-const getFirstAndLast = (array) => ({
-  first: array[0],
-  last: array[array.length - 1],
-});
-
-const concat = (arr1, arr2) => {
-  const arr = arr1.slice();
-  arr.push(...arr2);
-  return arr;
-};
-
-// Usage
-
-const schoolAges = [10, 12, 15, 15];
-const studentAges = [17, 18, 18, 19, 20];
-const ages = concat(schoolAges, studentAges);
-const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+console.log(UpperCase1.concat(UpperCase2)); // ["H", "E", "L", "L", "O", "W", "O", "R", "L", "D"]
