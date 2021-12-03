afed2f7925ba9225c403e7789d8ae6a0ee8b1baf
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-03 09:07:32 +0200

    Update lecture_notes/08 collections/a-union.py
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- lecture_notes/08 collections/a-union.py
+++ lecture_notes/08 collections/a-union.py
@@ -4,7 +4,7 @@ def union_cities(s1, s2):
     return result
 
 
-cities1 = ['Beijing', 'Kiev']
-cities2 = ['Kiev', 'London', 'Baghdad']
+cities1 = { 'Beijing', 'Kiev' }
+cities2 = { 'Kiev', 'London', 'Baghdad' }
 
 print(union_cities(cities1, cities2))
