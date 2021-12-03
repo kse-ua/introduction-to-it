85401054f7182043bab6f072fb508cc9bb1cc3e0
Uliana Makhno <94102483+UlianaMakhno@users.noreply.github.com>
2021-12-03 16:16:52 +0200

    Update 4-concat_optimization.js

--- 4-concat_optimization.js
+++ 4-concat_optimization.js
@@ -5,4 +5,4 @@ const concat = (arr1, arr2) => [...arr1, ...arr2];
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
-console.log(getFirstAndLast(ages));x
+console.log(getFirstAndLast(ages));
