6918ad336216ef1833e5d9ae592c3558bd1f300d
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 16:16:06 +0200

    Create 4-contact.js

---
+++ 4-contact.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Implementation
+
+const getFirstAndLast = (array) => ({
+  first: array[0],
+  last: array[array.length - 1],
+});
+
+const concat = (arr1, arr2) => {
+  return [...arr1, ...arr2];
+};
+
+// Usage
+
+const schoolAges = [10, 12, 15, 15];
+const studentAges = [17, 18, 18, 19, 20];
+const ages = concat(schoolAges, studentAges);
+console.log(getFirstAndLast(ages));
