2b72cb81890bbd82056a11de89c817b264762c9e
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-03 10:16:59 +0200

    Optimize + Example CONTACT

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -1,3 +1,21 @@
+const getFirstAndLast = (array) => ({
+  first: array[0],
+  last: array[array.length - 1],
+});
+
+const concat = (arr1, arr2) => {
+  const arr = arr1.slice();
+  arr.push(...arr2);
+  return arr;
+};
+
+const schoolAges = [10, 12, 15, 15];
+const studentAges = [17, 18, 18, 19, 20];
+const ages = concat(schoolAges, studentAges);
+const { first, last } = getFirstAndLast(ages);
+console.log({ first, last });
+
+
 // Exaple CONTACT
 const ArrLowerCase1 = ['h', 'e', 'l', 'l', 'o'];
 const ArrLowerCase2 = ['w', 'o', 'r', 'l', 'd'];
