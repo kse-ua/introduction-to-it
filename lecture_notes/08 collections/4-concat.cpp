9c37070900bb224ef8d0d690b858abc6ff6bfcb0
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-03 12:26:40 +0200

    tried to move concatenation to the function

--- lecture_notes/08 collections/4-concat.cpp
+++ lecture_notes/08 collections/4-concat.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int * concat(int array1[], int array2[], int length1, int length2) {
+  int length = length1 + length2;
+  int concated[length];
+  for (int i = 0; i < length1; i++) {
+    concated[i] = array1[i];
+  }
+
+  for (int i = 0; i < length2; i++) {
+    concated[i + length1] = array2[i];
+
+  return concated;
+  }
+
+}
+
 int main() {
   int schoolAges[] = { 10, 12, 15, 15 };
   int studentAges[] = { 17, 18, 18, 19, 20 };
@@ -7,15 +22,8 @@ int main() {
   int schoolLength = sizeof(schoolAges) / sizeof(int);
   int studentLength = sizeof(studentAges) / sizeof(int);
   int length = schoolLength + studentLength;
-  int ages[length];
 
-  for (int i = 0; i < schoolLength; i++) {
-    ages[i] = schoolAges[i];
-  }
-
-  for (int i = 0; i < studentLength; i++) {
-    ages[i + schoolLength] = studentAges[i];
-  }
+  int ages[length] = concat(schoolAges, studentAges, schoolLength, studentLength);
 
   for (int i = 0; i < length; i++) {
     printf("ages[%d]: %d\n", i, ages[i]);
