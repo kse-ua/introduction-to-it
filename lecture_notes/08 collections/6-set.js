8497d9df0ae2e130e17b6501e3714974fb799099
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-03 17:00:34 +0100

    Update 6-set.js
    Translated JavaScript to Python ("set" in JavaScript to "list" in Python)

--- lecture_notes/08 collections/6-set.js
+++ lecture_notes/08 collections/6-set.js
@@ -1,17 +1,10 @@
-'use strict';
+ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
+print(ages)
 
-const ages = new Set([10, 12, 15, 15, 17, 18, 18, 19, 20]);
-console.log({ ages });
+ages.append(16)
+ages.remove(20)
 
-ages.add(16);
-ages.delete(20);
+print(f"10: {10 in ages}, \n16: {16 in ages}, \n19: {19 in ages}, \n20: {20 in ages}")
 
-console.log({
-  10: ages.has(10),
-  16: ages.has(16),
-  19: ages.has(19),
-  20: ages.has(20),
-});
-
-ages.clear();
-console.log({ ages });
+ages.clear()
+print(ages)
