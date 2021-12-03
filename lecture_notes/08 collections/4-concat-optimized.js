ae3685aaf8692f40cd2740af6fe3768960cc5972
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-03 09:29:43 +0200

    Removed braces

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // Implementation
-const concat = (arr1, arr2) => (arr1.concat(arr2));
+const concat = (arr1, arr2) => arr1.concat(arr2);
 
 // Usage
 const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
+
 console.log({ ages });
