7e18bd4fc7b375a747ed6d83dd5221a890a4f31a
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-03 17:06:27 +0200

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,11 +1,8 @@
-let ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
-ages.splice(4, 1);
-let first = ages[0];
-let last = ages[-1];
-ages.splice(4, 1);
-ages.splice(5, 1, 'Boomer')
-ages.push('Alex')
-ages.shift()
-ages.unshift('Start')
-console.log({first, last});
-console.log(ages)
+'use strict';
+
+const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+
+const first = ages[0];
+const last = ages[ages.length - 1];
+
+console.log({ first, last });
