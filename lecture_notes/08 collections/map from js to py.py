54cd33f88f98d60508d56e3f5f9796a75d8dd379
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-03 10:01:24 +0200

    Translated code

---
+++ lecture_notes/08 collections/map from js to py.py
@@ -0,0 +1,13 @@
+NamesIq = {
+    'Vasia Pupkin': 19,
+    'Marcus Aurelius': 1860,
+}
+print(NamesIq)
+
+del NamesIq['Vasia Pupkin']
+print(NamesIq)
+
+print('Vasia Pupckin' in NamesIq, 'Marcus Aurelius' in NamesIq)
+
+NamesIq.clear()
+print(NamesIq)
