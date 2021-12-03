291497a28cc5c29660ba9fda571b09e366a43e0e
Iryna Sukhoiarska <94047966+ISukhoiarska@users.noreply.github.com>
2021-12-03 09:20:37 +0200

    Update 1-array.js

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -6,3 +6,19 @@ const first = ages[0];
 const last = ages[ages.length - 1];
 
 console.log({ first, last });
+
+ages.push(21);
+// add element to the end
+console.log(ages)
+
+ages.pop();
+// remove the last element
+console.log(ages)
+
+ages.unshift(1);
+// add element to the beginning
+console.log(ages)
+
+ages.shift(1);
+// remove element from the beginning
+console.log(ages)
