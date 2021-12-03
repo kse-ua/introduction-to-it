ddc923bc149ced64984d77250c9180f625a3ee87
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-03 09:59:47 +0200

    b-intersection.js => py

--- lecture_notes/08 collections/b-intersection.js
+++ lecture_notes/08 collections/b-intersection.js
@@ -1,23 +1,13 @@
-'use strict';
+intersection = lambda set1, set2: set1 & set2
 
-const intersection = (s1, s2) => {
-  const ds = [];
-  for (let i = 0; i < s1.length; i++) {
-    const item = s1[i];
-    if (s2.includes(item)) ds.push(item);
-  }
-  return ds;
-};
+cities1 = {'Beijing', 'Kiev'}
+cities2 = {'Kiev', 'London', 'Baghdad'}
 
-// const intersection = (s1, s2) => new Set(
-//   [...s1].filter((v) => s2.has(v))
-// );
+print({
+    'cities1': cities1,
+    'cities2': cities2
+})
 
-// Usage
+results = intersection(cities1, cities2)
+print(results)
 
-const cities1 = ['Beijing', 'Kiev'];
-const cities2 = ['Kiev', 'London', 'Baghdad'];
-console.dir({ cities1, cities2 });
-
-const results = intersection(cities1, cities2);
-console.dir(results);
