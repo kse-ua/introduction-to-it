f5b3a986b18bae7e38de8445201c3613d60bed24
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-03 09:58:35 +0200

    Update 4-concat-optimized.js

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Implementation
-function concat(arr1,arr2){
-    const arr = arr1.slice();
-    arr.push(...arr2);
-    return [arr[0],arr[arr.length - 1]]
-}
+function concat(arr1, arr2) {
+  const arr = arr1.slice();
+  arr.push(...arr2);
+  return [arr[0], arr[arr.length - 1]]
+};
 
 // Usage
 
