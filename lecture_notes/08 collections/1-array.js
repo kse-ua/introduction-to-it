a2323e1d301122aa516822243e62f6e7a0915e2c
Mykhailo Sherin <94047443+msherinn@users.noreply.github.com>
2021-12-03 13:42:43 +0200

    applied array methods, removed inunique elements from arrays

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+let ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
+
+// Implementation
+function onlyDistinct(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+// Usage
+ages = ages.filter(onlyDistinct);
+
+ages.unshift(8)
+ages.push(22)
 
 const first = ages[0];
 const last = ages[ages.length - 1];
--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -1,5 +1,10 @@
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 
+ages_set = set(ages)
+ages = list(ages_set)
+ages.insert(0, 8)
+ages.append(22)
+
 first = ages[0]
 last = ages[-1]
 
