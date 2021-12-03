9600c7c48d7f4cafdf094b122c2a35b5c70adc14
Iryna Sukhoiarska <94047966+ISukhoiarska@users.noreply.github.com>
2021-12-03 09:51:51 +0200

    Update 4-concat.js

--- lecture_notes/08 collections/4-concat.js
+++ lecture_notes/08 collections/4-concat.js
@@ -19,4 +19,4 @@ const schoolAges = [10, 12, 15, 15];
 const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
 const { first, last } = getFirstAndLast(ages);
-console.log({ first, last });
+console.log(getFirstAndLast(ages));
