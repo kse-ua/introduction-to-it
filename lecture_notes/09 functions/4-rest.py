3baf34eef3690005ab240d465c99af244a373e3a
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-05 12:05:08 +0200

    Create 4-rest.py

---
+++ lecture_notes/09 functions/4-rest.py
@@ -0,0 +1,18 @@
+def catch_rest(*args):
+    print(args)
+
+
+catch_rest(1, 2, 3)
+
+
+def f2(*args):
+    for element in args:
+        element_type = type(element)
+        print('Type', element_type)
+        if element_type == 'dict':
+            print('Value:', str(element))
+        else:
+            print('Value:', element)
+
+
+f2(1, 'Marcus', { 'field': 'value' })
