6fc0a5a0a5dfe4707c38824e85c5aaab54257122
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-08 11:27:58 +0200

    Add files via upload

---
+++ lecture_notes/09 functions/2-scope.cpp
@@ -0,0 +1,72 @@
+using namespace std;
+#include <iostream>
+
+string cities[] = {"Athens", "Roma", "London", "Beijing", "Kiev", "Riga"};
+int length = sizeof(cities)/sizeof(string);
+
+string toUpperCase(string s){
+    for(int i = 0; i < s.length(); i++)
+    {
+        s[i] = toupper(s[i]);
+    }
+    return s;
+}
+
+string toLowerCase(string s){
+    for(int i = 0; i < s.length(); i++)
+    {
+        s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
+void printArray(string s[], int length){
+    cout << "[ ";
+    for(int i = 0; i < length; i++){
+        cout << s[i] << ' ';
+    }
+    cout << "] " << endl;
+}
+
+void f1(){
+    string cities[] = {"Athens", "Roma"};
+    int length = sizeof(cities)/sizeof(string);
+    string upperCities[length];
+    string lowerCities[length];
+    for(int i = 0; i < length; i++){
+        upperCities[i] = toUpperCase(cities[i]);
+    }
+    for(int i = 0; i < length; i++){
+        lowerCities[i] = toLowerCase(cities[i]);
+    }
+
+    printArray(cities, length);
+    printArray(upperCities, length);
+
+    printArray(cities, length);
+    printArray(lowerCities, length);
+
+    {
+        string cities[] = {"London", "Beijing", "Kiev"};
+        length = sizeof(cities)/sizeof(string);
+        string upperCities[length];
+        for(int i = 0; i < length; i++){
+            upperCities[i] = toUpperCase(cities[i]);
+        }
+        printArray(cities, length);
+        printArray(upperCities, length);
+    }
+}
+
+int main() {
+    f1();
+    printArray(cities, length);
+    int lengthArr[length];
+
+    cout << "[ ";
+    for(int i = 0; i < length; i++){
+        cout << cities[i].length() << " ";
+    }
+    cout << "]" << endl;
+}
+
