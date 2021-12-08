907c22dc79d5c170eebd57c7f3cc78bce7e88920
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-08 10:34:58 +0200

    Create 2-scope.py

---
+++ lecture_notes/09 functions/2-scope.py
@@ -0,0 +1,24 @@
+cities = ['Athens', 'Roma', 'London', 'Beijing', 'Kiev', 'Riga']
+def f(s):
+    return len(s)
+
+def f1():
+    cities = ['Athens', 'Roma']
+    f = lambda s: s.upper()
+    
+    print(cities)
+    print(list(map(f, cities)))
+
+    f = lambda s: s.lower()
+    print(cities)
+    print(list(map(f, cities)))
+
+    f = lambda s: s.upper()
+    cities = ['London', 'Beijing', 'Kiev']
+    print(cities)
+    print(list(map(f, cities)))
+
+f1()
+
+print(cities)
+print(list(map(f, cities)))
