ed131cf41a41e9623eff2796564ec11ee0b43928
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-08 22:38:21 +0200

    succesfully translated function

---
+++ lecture_notes/09 functions/3-default.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max(int a = 0, int b = 0) {
+    if(a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main () {
+    printf("10,20: %d\n", max(10, 20));
+    printf("10: %d\n", max(10));
+    printf("-20: %d\n", max(-20));
+}
