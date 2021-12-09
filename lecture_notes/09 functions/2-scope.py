e2d461caa882663955d06b1c58e27d04239326be
Egor Belous <belousegor5@gmail.com>
2021-12-10 01:15:54 +0200

    translate 2-scope.js to python

---
+++ lecture_notes/09 functions/2-scope.py
@@ -0,0 +1,49 @@
+cities = ['Athens', 'Roma', 'London', 'Beijing', 'Kiev', 'Riga']
+f = lambda s: len(s) 
+
+def f1():
+    cities = ['Athens', 'Roma']
+    f = lambda s: s.upper()
+    
+    print('{cities: ' + str(cities) + '}')
+    print(map(f, cities ))
+
+
+    def f1_1():
+        f = lambda s: s.lower()
+
+        print('{cities: ' + str(cities) + '}')
+        print(map(f, cities ))
+    f1_1()
+
+    def f1_2():
+        cities = ['London', 'Beijing', 'Kiev']
+        
+        print('{cities: ' + str(cities) + '}')
+        print(map(f, cities ))
+    f1_2()
+f1()   
+
+print('{cities: ' + str(cities) + '}')
+print(map(f,cities ))
+    
+
+
+
+         
+    
+
+
+
+
+
+
+   
+
+
+
+
+
+
+   
+
