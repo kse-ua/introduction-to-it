41ddbe7292b4e2f6cc850e140faf5b71d107876c
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-09 22:11:16 +0200

    Added alternative concat

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -1,7 +1,20 @@
 'use strict';
 
 // Implementation
-const concat = (arr1, arr2) => arr1.concat(arr2);
+const concat = (arr1, arr2) => arr1.concat(arr2)
+
+const altConcat = (arr1, arr2) => {
+const arr = [];
+// arr.push(arr1);
+// arr.push(arr2);
+for (const value of arr1) {
+    arr.push(value) 
+}
+for (const value of arr2) {
+    arr.push(value)
+}
+return arr;
+}
 
 // Usage
 const schoolAges = [10, 12, 15, 15];
@@ -9,3 +22,6 @@ const studentAges = [17, 18, 18, 19, 20];
 const ages = concat(schoolAges, studentAges);
 
 console.log({ ages });
+
+const arr = altConcat(schoolAges, studentAges);
+console.log({ arr })
