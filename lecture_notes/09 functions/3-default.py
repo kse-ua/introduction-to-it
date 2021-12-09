d7e837dcfe8f2d8a4035da7cb754ad1608c75bbe
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-09 19:26:09 +0200

    Update 3-default.py

--- lecture_notes/09 functions/3-default.py
+++ lecture_notes/09 functions/3-default.py
@@ -1,5 +1,10 @@
+# №1
 def maximum(a = 0, b = 0):
     return max(a, b)
 
+print(maximum(10, 20), maximum(10), maximum(-20))
 
+
+# №2
+max = lambda a = 0, b = 0: a if a > b else b
 print(maximum(10, 20), maximum(10), maximum(-20))
