a872c2233faefaf83197406a15a0977d9f622243
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-09 21:17:43 +0200

    Translated from 5-procedure.js

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,28 @@
+def addFunction(a, b):
+    res = a + b
+    return res
+
+
+cache = {}
+
+
+def addProcedure(a, b):
+    key = f'{a}, {b}'
+    res = a + b
+    cache[key] = res
+    return res
+
+
+print([
+    addFunction(10, 20),
+    addFunction(1, 2),
+    addFunction(100, 20),
+    addFunction(100, 200)
+])
+
+print([
+    addProcedure(10, 20),
+    addProcedure(1, 2),
+    addProcedure(100, 20),
+    addProcedure(100, 200),
+])
