87ccaefb06962838a65e3b1a6f02d7353f96f069
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-09 19:46:42 +0200

    Create 2-scope.cpp

---
+++ lecture_notes/09 functions/2-scope.cpp
@@ -0,0 +1,40 @@
+#include <iostream> // input and output info from out code
+using namespace std;  // Declaring on a global level
+
+void mainOutput() {
+    string cities[] = {"Athens", "Roma", "London", "Beijing", "Kiev", "Riga"};
+
+    for (int i = 0; i < 5; i++) { cout << cities[i].size() << endl; }
+
+}
+
+int main() {
+    mainOutput();
+
+    string FirstCities[] = {"Athens", "Roma"};
+
+    for (auto & citie : FirstCities) {
+        transform(citie.begin(), citie.end(), citie.begin(), ::toupper);
+        cout << citie << endl;
+    }
+
+    cout << endl;
+
+    for (auto & citie : FirstCities) {
+        transform(citie.begin(), citie.end(), citie.begin(), ::tolower);
+        cout << citie << endl;
+    }
+
+    cout << endl;
+
+    string ThirdCities[] = {"London", "Beijing", "Kiev"};
+
+    for (auto & citie : ThirdCities) {
+        transform(citie.begin(), citie.end(), citie.begin(), ::toupper);
+        cout << citie << endl;
+    }
+
+    return 0;
+}
+
+
