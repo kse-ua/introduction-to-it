e433a560650d22d9933a8128321c3a8d65d69856
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-09 21:59:31 +0200

    Create 5-procedure.py

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,21 @@
+def addFunction(a, b):
+    result = a + b
+    return result
+cache = {}
+def addProcedure(a, b):
+    key = str(a) + ',' + str(b)
+    if key in cache:
+        return cache[key]
+    res = a + b
+    cache[key] = res
+    return res
+print(
+    addFunction(10, 20),
+    addFunction(1, 2),
+    addFunction(100, 20),
+    addFunction(100, 200))
+print(
+    addProcedure(10, 20),
+    addProcedure(1, 2),
+    addProcedure(100, 20),
+    addProcedure(100, 200),)
