8bc1b260f4a6d7726625ae0e2be8c359158e0334
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-10 01:17:00 +0200

    fighting with dictionaries

--- lecture_notes/09 functions/5-procedure.py
+++ lecture_notes/09 functions/5-procedure.py
@@ -6,15 +6,13 @@ cache = {}
 
 def addProcedure (a, b):
     key = str(a) + ", " + str(b)
-    res = 
-    if res:
+    res = cache.update({key})
+    if cache.get(key) != 0:
         return res
     res = a + b
     cache[key] = res
-    # print(cache)
+    print(cache)
     return res
 
-addProcedure(3, 4)
-addProcedure(1, 2)
-addProcedure(100, 20)
-addProcedure(100, 200)
+print(addProcedure(3, 4))
+print(addProcedure(3, 4))
---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,20 @@
+cache = {}
+
+def addProcedure (a, b):
+    key = str(a) + ", " + str(b)
+    res = 
+    if res:
+        return res
+    res = a + b
+    cache[key] = res
+    return res
+
+def subProcedure (a, b):
+    key = str(a) + ", " + str(b)
+    res = 
+    if res:
+        return res
+    res = a - b
+    cache[key] = res
+    return res
+
