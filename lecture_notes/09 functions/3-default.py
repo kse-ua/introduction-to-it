3c00f55d8d39503a3559135ae00159641791ffbc
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-09 19:42:42 +0200

    Update 3-default.py

--- lecture_notes/09 functions/3-default.py
+++ lecture_notes/09 functions/3-default.py
@@ -1,6 +1,9 @@
 # №1
-def maximum(a = 0, b = 0):
-    return max(a, b)
+def max(a = 0, b = 0):
+    if a > b:
+        return a
+    else:
+        return b
 
 print(maximum(10, 20), maximum(10), maximum(-20))
 
