a6887e84525515cc575ab81d3c2f4bb3637e3873
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-10 23:52:55 +0200

    Update 6-shared.cpp

--- lecture_notes/09 functions/6-shared.cpp
+++ lecture_notes/09 functions/6-shared.cpp
@@ -7,14 +7,12 @@ int subProcedure(int a, int b);
 
 map<string, int> cache;
 
-int main()
-{
+int main(){
     cout << "sub: " << subProcedure(5, 2) << endl;
     cout << "add: " << addProcedure(5, 2) << endl;
 }
 
-int addProcedure(int a, int b)
-{
+int addProcedure(int a, int b){
     string key = to_string(a) + ',' + to_string(b);
     if (cache.count(key) == 1)
     {
@@ -25,8 +23,7 @@ int addProcedure(int a, int b)
     return res;
 }
 
-int subProcedure(int a, int b)
-{
+int subProcedure(int a, int b){
     string key = to_string(a) + ',' + to_string(b);
     if (cache.count(key) == 1)
     {
