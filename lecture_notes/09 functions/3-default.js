e9b74b21869c1c20c113e73ab12a77c4dc4c7de1
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-10 09:18:48 +0200

    Update 3-default.js

--- lecture_notes/09 functions/3-default.js
+++ lecture_notes/09 functions/3-default.js
@@ -1,5 +1,17 @@
+""""
 'use strict';
 
 const max = (a = 0, b = 0) => (a > b ? a : b);
 
 console.log([max(10, 20), max(10), max(-20)]);
+"""
+
+# ----------------------------------------------------------------------
+
+def max_def_v1(a=0, b=0):
+    result = a if a > b else b
+    return result
+
+
+max = max_def_v1
+print([max(10, 20), max(10), max(-20)])
