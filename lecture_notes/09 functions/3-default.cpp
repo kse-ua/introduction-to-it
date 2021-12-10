b56ffe265cd73de218976d26684f40308a59151f
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-10 13:26:09 +0200

    Create 3-default.cpp

---
+++ lecture_notes/09 functions/3-default.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Function that add two numbers
+int maximum(int a = 0, int b = 0)
+{
+    if (a > b) {
+        return a;
+    }
+
+    if (b > a) {
+        return b;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    cout << maximum(-20) << '\n';
+    return 0;
+}
