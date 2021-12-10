57b6d57e0b19e298eb5caef785e85c9be8644ba9
istepmother <imachukha@kse.org.ua>
2021-12-10 09:53:51 +0200

    added 2-scope.cpp

---
+++ lecture_notes/09 functions/2-scope.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+int f(char s[])
+{
+    int length = sizeof(s) / sizeof(char);
+    return length;
+}
+
+int f1(char s[])
+{
+    int length = sizeof(s) / sizeof(char);
+    return length;
+}
+
+
+
+void f1(){
+    char cities[][7] = {'Athens', 'Roma'};
+
+}
+
+
+int main()
+{
+    char cities[][16] = {'Athens', 'Roma', 'London', 'Beijing', 'Kiev', 'Riga'};
+
+    return 0;
+}
