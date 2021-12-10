b9391e13962acdb22d6e9810f003674baaad76cc
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-10 10:32:36 +0200

    6-shared c++ and python

---
+++ lecture_notes/09 functions/6-shared.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+
+// Created map
+// (key, value) like object in js
+std::map<std::string, int> cache;
+
+int addProcedure (int a, int b) {
+    std::string key = a + "," + b;
+
+    if (cache.contains(key)){
+        return cache[key];
+    }
+
+    int res = a + b;
+    cache[key] = res;
+    return res;
+};
+
+
+int subProcedure (int a, int b) {
+    std::string key = a + "," + b;
+
+    if (cache.contains(key)){
+        return cache[key];
+    }
+
+    int res = a - b;
+    cache[key] = res;
+    return res;
+
+};
+
+
+int main(){
+    printf("%d\n", subProcedure(5, 2));
+    printf("%d\n", addProcedure(5, 2));
+    return 0;
+}
---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,22 @@
+cache = {}
+
+def add_procedure(a, b):
+    global cache
+    key = str(a), str(b)
+    if key in cache:
+        return cache[key]
+    res = a + b
+    cache[key] = res
+    return res
+
+def sub_procedure(a, b):
+    global cache
+    key = str(a), str(b)
+    if key in cache:
+        return cache[key]
+    res = a - b
+    cache[key] = res
+    return res
+
+print("sub:", sub_procedure(5, 2))
+print("add:", add_procedure(5, 2))
