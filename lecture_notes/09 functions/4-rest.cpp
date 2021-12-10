4eae671d2f6c3570a7a5bd11c45d289b7ad77433
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-10 10:30:38 +0200

    wrote the algorith of massive of args

--- lecture_notes/09 functions/4-rest.cpp
+++ lecture_notes/09 functions/4-rest.cpp
@@ -1,5 +1,21 @@
-#include <stdio.h>
+using namespace std;
+#include <iostream>
+#include <stdarg.h> 
 
-int catchrest ()
+void catchRest(int length, ...)
+{
+     va_list args;
+     va_start(args, length);
+     for(int i = 0; i < length; i++)
+     {
+         int value = va_arg(args, int);
+         cout << value << " ";
+     }
+     cout << endl;
+     va_end(args);
+}
 
-// how to get a lot of ars in c++??
+int main() {
+    catchRest(4, 4, 6, 3, 7);
+    // it makes no sense to get the type of args as we declare it alresdy
+}
