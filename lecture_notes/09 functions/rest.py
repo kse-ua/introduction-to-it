be78c62a23f4bb49c30dc876ee021f1289edec59
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-10 10:03:49 +0200

    Create rest.py

---
+++ lecture_notes/09 functions/rest.py
@@ -0,0 +1,10 @@
+def catchRest(*args):
+    print(args)
+    
+def f2(*args):
+    for arg in args:
+        print(type(arg))
+        print(arg)
+
+catchRest(1, 2, 3)
+f2(1, 'Marcus', { "field": 'value' })
