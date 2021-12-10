e575a26164e6bd29873fad4f82342dfa189bb321
Anastasia <anastasia@Anastasias-MacBook-Air.local>
2021-12-10 11:24:48 +0200

    Corrected indentations

--- lecture_notes/08 collections/4-concat-optimized.js
+++ lecture_notes/08 collections/4-concat-optimized.js
@@ -4,11 +4,11 @@
 const concat = (arr1, arr2) => arr1.concat(arr2);
 
 const altConcat = (arr1, arr2) => {
-    const arr = [];
-    arr.push(...arr1);
-    arr.push(...arr2);
-    return arr;
-    };
+  const arr = [];
+  arr.push(...arr1);
+  arr.push(...arr2);
+  return arr;
+};
 
 // Usage
 const schoolAges = [10, 12, 15, 15];
