c2c2e222464908ba318510b822e1f0e87696919d
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-10 09:25:12 +0200

    Update and rename 5-procedure.js to 5-procedure.py

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,33 @@
+def addFunction(a, b):
+    res = a + b
+    return res
+
+
+cache = {}  # dictionary
+
+
+def addProcedure(a, b):
+    key = [str(a) + ',' + str(b)]
+    if cache == {}:
+        res = cache.fromkeys(key)  # create a dictionary with a key
+    else:
+        res = cache
+    if res.get(key[0]) != None:
+        return res.get(key[0])  # returns the value of the key, but if there is none, then None
+    res = a + b
+    cache[key[0]] = res
+    return res
+
+
+print([
+    addFunction(10, 20),
+    addFunction(1, 2),
+    addFunction(100, 20),
+    addFunction(100, 200),
+])
+print([
+    addProcedure(10, 20),
+    addProcedure(1, 2),
+    addProcedure(100, 20),
+    addProcedure(100, 200),
+])
