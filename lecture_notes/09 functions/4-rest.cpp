f455eac8ad8e49cd6dcd16e1107fc2e9e4b56064
Khrystyna Petrynka <90193412+kpetrynka@users.noreply.github.com>
2021-12-11 19:00:04 +0200

    Update lecture_notes/09 functions/4-rest.cpp
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- lecture_notes/09 functions/4-rest.cpp
+++ lecture_notes/09 functions/4-rest.cpp
@@ -6,8 +6,7 @@ void catchRest(int length, ...)
 {
      va_list args;
      va_start(args, length);
-     for(int i = 0; i < length; i++)
-     {
+     for (int i = 0; i < length; i++) {
          int value = va_arg(args, int);
          cout << value << " ";
      }
