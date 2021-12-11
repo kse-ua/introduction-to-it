71c65ce3915a866092aeae830243313c0edce51d
bugarenko <tbuharenko@kse.org.ua>
2021-12-11 16:44:48 +0200

    6-shared.py

---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,20 @@
+cache = {}
+
+def addProcedure(a, b):
+     key = str(a) + ',' + str(b)
+     if key in cache:
+         return cache[key]
+     res = a + b
+     cache[key] = res
+     return res
+
+def subProcedure(a, b):
+     key = str(a) + ',' + str(b)
+     if key in cache:
+         return cache[key]
+     res = a - b
+     cache[key] = res
+     return res
+
+print('sub:', subProcedure(5, 2))
+print('add:', addProcedure(5, 2))
