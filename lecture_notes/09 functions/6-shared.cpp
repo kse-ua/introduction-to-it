e457e43da75e4fda5ac0daad3f636e7dab419881
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-11 21:47:49 +0200

    Done

--- lecture_notes/09 functions/6-shared.cpp
+++ lecture_notes/09 functions/6-shared.cpp
@@ -4,28 +4,35 @@ using namespace std;
 
 int subProcedure(int a, int b) {
 
-    map <string, int> cache;
+    map < string, int > cache;
     std::string keyValue = std::to_string(a) + "," + std::to_string(b);
-    if (cache.empty())
-        int res = 0;
-    int res = a - b;
+
+    cache[keyValue] = 0;
+    int res = cache[keyValue];
+    if (res != 0) {
+        return res;
+    }
+    res = a - b;
     cache[keyValue] = res;
     return res;
 }
 
 int addProcedure(int a, int b) {
 
-    map <string, int> cache;
+    map < string, int > cache;
     std::string keyValue = std::to_string(a) + "," + std::to_string(b);
-    if (cache.empty())
-        int res = 0;
-    int res = a + b;
+
+    cache[keyValue] = 0;
+    int res = cache[keyValue];
+    if (res != 0) {
+        return res;
+    }
+    res = a + b;
     cache[keyValue] = res;
     return res;
 }
 
-int main ()
-{
+int main() {
     cout << "addProcedure: " << addProcedure(10, 20) << endl;
     cout << "addFunction: " << subProcedure(10, 20) << endl;
     cout << "addProcedure: " << addProcedure(100, 20) << endl;
