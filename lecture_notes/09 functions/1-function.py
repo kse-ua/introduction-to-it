52fb6113152d51c29199aaea825116f956f03f23
Kate732 <kmaistrenko@kse.org.ua>
2021-12-11 16:49:12 +0200

    1-f

---
+++ lecture_notes/09 functions/1-function.py
@@ -0,0 +1,10 @@
+def add1(first_num, sec_num):
+    return first_num + sec_num
+
+
+def add2(first_num, sec_num):
+    res = first_num + sec_num
+    return res
+
+
+print(f" {add1(10, 20)}, {add2(10, 20)}")
