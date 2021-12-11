bec2064efbec09d967cdac88aecefef0c968a344
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-11 23:19:30 +0200

    Update 5-procedure.cpp

--- lecture_notes/09 functions/5-procedure.cpp
+++ lecture_notes/09 functions/5-procedure.cpp
@@ -7,22 +7,21 @@ int addProcedure(int a, int b);
 
 map<string, int> cache;
 
-int main(){
+int main() {
     cout << addFunction(10, 20) << " " << addFunction(1,2) << " ";
     cout << addFunction(100, 20) << " " << addFunction(100, 200) << endl;
     cout << addProcedure(10, 20) << " " << addProcedure(1,2) << " ";
     cout << addProcedure(100, 20) << " " << addProcedure(100, 200) << endl;
 }
 
-int addFunction(int a, int b){
+int addFunction(int a, int b) {
     int res = a + b;
     return res;
 }
 
-int addProcedure(int a, int b){
+int addProcedure(int a, int b) {
     string key = to_string(a) + ',' + to_string(b);
-    if (cache.count(key) == 1)
-    {
+    if (cache.count(key) == 1) {
         return cache[key];
     }
     int res = a + b;
