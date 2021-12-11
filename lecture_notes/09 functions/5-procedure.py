c7011ca023c6eed7e27c6eb2622c2ae60c5a2bda
bugarenko <tbuharenko@kse.org.ua>
2021-12-11 16:35:02 +0200

    5-procedure.py

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,25 @@
+def addFunction(a, b):
+     return a + b
+
+cache = {}
+
+def addProcedure(a, b):
+     key = f"{a}, {b}"
+     res = a + b
+     cache[key] = res
+     return res
+
+print([
+    addFunction(10, 20),
+    addFunction(1, 2),
+    addFunction(100, 20),
+    addFunction(100, 200)
+ ])
+
+print([
+    addProcedure(10, 20),
+    addProcedure(1, 2),
+    addProcedure(100, 20),
+    addProcedure(100, 200),
+ ])
+
