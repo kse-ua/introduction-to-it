4d0de23db85a80d30ca928477e6e27211b6d6627
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-11 21:46:25 +0200

    Done

--- lecture_notes/09 functions/5-procedure.cpp
+++ lecture_notes/09 functions/5-procedure.cpp
@@ -2,28 +2,30 @@
 #include <map>
 using namespace std;
 
-int addFunction (int a, int b) {
+int addFunction(int a, int b) {
     int res = a + b;
     return res;
 }
 
 int addProcedure(int a, int b) {
 
-    map <string, int> cache;
+    map < string, int > cache;
     std::string keyValue = std::to_string(a) + "," + std::to_string(b);
-    if (cache.empty())
-        int res = 0;
-    int res = a + b;
+
+    cache[keyValue] = 0;
+    int res = cache[keyValue];
+    if (res != 0) {
+        return res;
+    }
+    res = a + b;
     cache[keyValue] = res;
     return res;
 }
 
-int main ()
-{
+int main() {
     cout << "addProcedure: " << addProcedure(10, 20) << endl;
     cout << "addFunction: " << addFunction(10, 20) << endl;
     cout << "addProcedure: " << addProcedure(100, 20) << endl;
     cout << "addFunction: " << addFunction(100, 20) << endl;
     return 0;
 }
-
