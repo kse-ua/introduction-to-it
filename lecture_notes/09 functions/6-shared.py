2b46a7300325188f6839c067523f6a2971f20c4a
Mykhailo Sherin <94047443+msherinn@users.noreply.github.com>
2021-12-12 17:16:29 +0200

    translated from 6-shared.js

---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,23 @@
+cache = {}
+
+
+def add_procedure(a, b):
+    key = f'{a}, {b}'
+    if key in cache:
+        return cache[key]
+    result = a + b
+    cache[key] = result
+    return result
+
+
+def sub_procedure(a, b):
+    key = f'{a}, {b}'
+    if key in cache:
+        return cache[key]
+    result = a - b
+    cache[key] = result
+    return result
+
+
+print({'sub': sub_procedure(5, 2)})
+print({'add': add_procedure(5, 2)})
