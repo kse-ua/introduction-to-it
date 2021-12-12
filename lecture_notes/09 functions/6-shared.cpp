9db90e2bc63cebb877b51bb633b2995e6ed8c9af
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-12 21:50:31 +0200

    Translation

---
+++ lecture_notes/09 functions/6-shared.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+#include <iostream>
+#include <map>
+
+int addProcedure(int a, int b);
+int subProcedure(int a, int b);
+
+map<string, int> cache;
+
+int main() {
+    cout << "sub: " << subProcedure(5, 2) << endl;
+    cout << "add: " << addProcedure(5, 2) << endl;
+}
+
+int addProcedure(int a, int b) {
+    string key = to_string(a) + ',' + to_string(b);
+    if (cache.count(key) == 1) {
+        return cache[key];
+    }
+    int res = a + b;
+    cache[key] = res;
+    return res;
+}
+
+int subProcedure(int a, int b) {
+    string key = to_string(a) + ',' + to_string(b);
+    if (cache.count(key) == 1) {
+        return cache[key];
+    }
+    int res = a - b;
+    cache[key] = res;
+    return res;
+}
+
