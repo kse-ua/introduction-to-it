42076a978096e00c543758fcf74dc752984627fe
Kate732 <kmaistrenko@kse.org.ua>
2021-12-12 14:56:39 +0200

    4-rest

---
+++ lecture_notes/09 functions/4-rest.py
@@ -0,0 +1,19 @@
+import json
+
+def catchRest(*args):
+   print(args)
+
+catchRest(1, 2, 3)
+#"""
+def f2(*args):
+    for arg in args:
+        type_arg = ""
+        type_arg = type(arg)
+        print('Type: ', type_arg)
+        if type == dict:
+            print('Value: ', json.dumps(arg))
+        else:
+            print('Value: ', arg)
+
+f2(1, 'Marcus', { 6: 'value' })
+#"""
