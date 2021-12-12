8ce51812de75acf4846178ba04645b4801b5a18e
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-12 21:19:18 +0100

    Create 5-procedure.cpp

---
+++ lecture_notes/09 functions/5-procedure.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+#include <iostream>
+#include <map>
+
+map<string, int> cache;
+
+int addFunction(int a, int b) {
+    int res = a + b;
+    return res;
+}
+
+int addProcedure(int a, int b) {
+    string key = to_string(a) + ',' + to_string(b);
+    if (cache.count(key) == 1) {
+        return cache[key];
+    }
+    int res = a + b;
+    cache[key] = res;
+    return res;
+}
+
+int main() {
+    cout << "[ " << addFunction(10, 20) << ", " << addFunction(1,2) << ", ";
+    cout << addFunction(100, 20) << ", " << addFunction(100, 200) << " ]" << endl;
+    cout << "[ " << addProcedure(10, 20) << ", " << addProcedure(1,2) << ", ";
+    cout << addProcedure(100, 20) << ", " << addProcedure(100, 200) << " ]";
+}
