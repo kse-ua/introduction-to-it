d8f03bc8016807a9f2335b1e5da6dffb8e754e17
Uliana Makhno <94102483+UlianaMakhno@users.noreply.github.com>
2021-12-12 22:53:03 +0200

    Create 3-default.py

---
+++ lecture_notes/09 functions/3-default.py
@@ -0,0 +1,23 @@
+num1 = [0,0]
+
+result = (num1[0] if num1[0] > num1[1] else num1[1])
+
+print(result)
+
+num2 = [10,20]
+
+result = (num2[0] if num2[0] > num2[1] else num2[1])
+
+print(result)
+
+num3 = [10,0]
+
+result = (num3[0] if num3[0] > num3[1] else num3[1])
+
+print(result)
+
+num4 = [0,-20]
+
+result = (num4[0] if num4[0] > num4[1] else num4[1])
+
+print(result)
