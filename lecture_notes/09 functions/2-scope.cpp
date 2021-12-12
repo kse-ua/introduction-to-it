a430f7b2c4db49834df83ef1c90e765887dcbd6b
istepmother <imachukha@kse.org.ua>
2021-12-12 21:16:08 +0200

    added cpp for task 2

--- lecture_notes/09 functions/2-scope.cpp
+++ lecture_notes/09 functions/2-scope.cpp
@@ -1,24 +1,69 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
+string cities[] = {"Athens", "Roma", "London", "Beijing", "Kiev", "Riga"};
+int len = sizeof(cities) / sizeof(string);
 
-int f(char s[]) {
-    int length = sizeof(s) / sizeof(char);
-    return length;
+void print_cities(string s[], int len) {
+    cout << "cities: [ ";
+    for(int idx = 0; idx < len; idx++) {
+        cout << s[idx] << " ";
+    }
+    cout << "]\n";
 }
 
-int f1(char s[]) {
-    int length = sizeof(s) / sizeof(char);
-    return length;
+void print_len() {
+    cout << "[ ";
+    for(int i = 0; i < len; i++) {
+        cout << cities[i].size() << " ";
+    }
+    cout << "]\n";
+}
+
+string to_upper(string s) {
+    for(int i = 0; i < s.size(); i++){
+        s[i] = toupper(s[i]);
+    }
+    return s;
+}
+
+string to_lower(string s) {
+    for(int i = 0; i < s.size(); i++){
+        s[i] = tolower(s[i]);
+    }
+    return s;
 }
 
 void f1() {
-    char cities[][7] = {'Athens', 'Roma'};
+    string cities[] = {"Athens", "Roma"};
+    int len = sizeof(cities)/sizeof(string);
+    string upper[len];
+    for (int idx = 0; idx < len; idx++) {
+        upper[idx] = to_upper(cities[idx]);
+    }
+    print_cities(cities, len);
+    print_cities(upper, len);
+    string lower[len];
+    for (int idx = 0; idx < len; idx++) {
+        lower[idx] = to_lower(cities[idx]); 
+    }
+    print_cities(cities, len);
+    print_cities(lower, len);
+    {
+        string cities[] = {"London", "Beijing", "Kiev"};
+        len = sizeof(cities)/sizeof(string);
+        string upper[len];
+        for(int idx = 0; idx < len; idx++) {
+            upper[idx] = to_upper(cities[idx]);
+        }
+        print_cities(cities, len);
+        print_cities(upper, len);
+    }
 }
 
 int main() {
-    char cities[][16] = {'Athens', 'Roma', 'London', 'Beijing', 'Kiev', 'Riga'};
+    f1();
+    print_cities(cities, len);
+    print_len();
     return 0;
 }
