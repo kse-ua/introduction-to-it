ce1470b4078f08708788f7001fbf645c09838eec
Yaroslav Melnichuk <94102798+LieTieOsu@users.noreply.github.com>
2021-12-12 22:02:53 +0200

    Delete lecture_notes/08 collections directory

--- lecture_notes/08 collections/1-list.py
+++
@@ -1,9 +0,0 @@
-ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-first = ages[0]
-last = ages[-1]
-ages.insert(5, "kid")
-ages.remove(18)
-ages.pop()
-ages.reverse()
-print({'first': first, 'last': last})
-print(ages)
