0d2819c366c3817051dadf60fb33eddf5bdc6525
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-12 14:12:14 +0200

    Update 3-default.cpp

--- lecture_notes/09 functions/3-default.cpp
+++ lecture_notes/09 functions/3-default.cpp
@@ -2,16 +2,11 @@
 using namespace std;
 
 int max(int a = 0, int b = 0) {
-    if (a > b) {
-        return a;
-    }
-    else {
-        return b;
-    }
+    int larger = (a > b) ? a : b;
 }
 int main()
 {
-cout << max(20,10) << endl;
-cout << max(10, 20) << endl;
-cout << max(-20) << endl;
+    cout << max(20,10) << endl;
+    cout << max(10, 20) << endl;
+    cout << max(-20) << endl;
 }
