5cc0d5d1ef39796ac48643e8e9c7e11e1eb87471
Хомич Ярослав <94047397+YaroslavKSE@users.noreply.github.com>
2021-12-12 13:07:29 +0200

    Create 2-scope.cpp

---
+++ lecture_notes/09 functions/2-scope.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+
+#include <iostream>
+
+string cities[] = {"Athens", "Roma", "London", "Beijing", "Kiev", "Riga"};
+int length = sizeof(cities) / sizeof(string);
+
+string UpperCase(string world) {
+    for (int i = 0; i < world.length(); i++) {
+        world[i] = toupper(world[i]);
+    }
+    return world;
+}
+
+string LowerCase(string world) {
+    for (int i = 0; i < world.length(); i++) {
+        world[i] = tolower(world[i]);
+    }
+    return world;
+}
+
+void printArray(string world[], int length) {
+    cout << "[ ";
+    for (int i = 0; i < length; i++) {
+        cout << world[i] << ' ';
+    }
+    cout << "] " << endl;
+}
+
+int main() {
+    printArray(cities, length);
+    cout << UpperCase(cities[0]) << ' ' << UpperCase(cities[1]) << endl;
+    cout << LowerCase(cities[0]) << ' ' << LowerCase(cities[1]) << endl;
+    cout << UpperCase(cities[2]) << ' ' << UpperCase(cities[3]) << ' ' << UpperCase(cities[4]) << endl;
+    cout << cities[0].length() << ' ' << cities[1].length() << ' ' << cities[2].length() << ' ' << 
+    cities[3].length() << ' ' << cities[4].length() << ' ' << cities[5].length() << endl;
+    return 0;
+}
