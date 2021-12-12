6d5c3fa434b92ad103f4a80f7aa513f27bd5f6b6
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-12 19:55:30 +0200

    Create 6 - shared.py

---
+++ lecture_notes/09 functions/6 - shared.py
@@ -0,0 +1,23 @@
+cache = {}
+
+
+def add_procedure(a, b):
+    key = f'{str(a)},{str(b)}'
+    if key in cache:
+        return cache[key]
+    res = a + b
+    cache[key] = res
+    return res
+
+
+def sub_procedure(a, b):
+    key = f'{str(a)},{str(b)}'
+    if key in cache:
+        return cache[key]
+    res = a - b
+    cache[key] = res
+    return res
+
+
+print(sub_procedure(5, 2))
+print(add_procedure(5, 2))
