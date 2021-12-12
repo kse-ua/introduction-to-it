7da94349e7395efed66a6bf7c1b3b9bb973912bc
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2021-12-12 19:13:53 +0200

    done 6-shared.py

---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,19 @@
+cache = {}
+
+
+def add_procedure(a, b):
+    key = f'{a}, {b}'
+    res = a + b
+    cache[key] = res
+    return res
+
+
+def sub_procedure(a, b):
+    key = f'{a}, {b}'
+    res = a - b
+    cache[key] = res
+    return res
+
+
+print('sub_procedure:', sub_procedure(5, 2))
+print('add_procedure:', add_procedure(5, 2))
