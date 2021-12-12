79eb4b6558a1f0477d88003095484c4f47ea3404
Liza Machukha <94099022+istepmother@users.noreply.github.com>
2021-12-12 14:54:25 +0200

    Add array examples

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -6,18 +6,3 @@ const first = ages[0];
 const last = ages[ages.length - 1];
 
 console.log({ first, last });
-
-ages.push(-1003);
-console.log(ages[ages.length - 1]);
-
-ages.pop();
-console.log(ages[ages.length - 1]);
-
-ages.unshift(-832);
-console.log(ages[0]);
-
-ages.shift();
-console.log(ages[0]);
-
-ages.sort();
-console.log(ages);
