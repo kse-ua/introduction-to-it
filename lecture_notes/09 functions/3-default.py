025dacbf2434ef3fb15ca8e9a2d27747180d57ce
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-12 21:06:33 +0200

    Create 3-default.py

---
+++ lecture_notes/09 functions/3-default.py
@@ -0,0 +1,11 @@
+def max(x=0, y=0):
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    elif x == y:
+        print("equal")
+        return
+
+
+print(max(10, 20), max(10), max(-20))
