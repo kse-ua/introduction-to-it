8fdf66d7bc529c700e88a219155a6f16dc8c6adf
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-13 22:17:41 +0200

    Updated

--- lecture_notes/09 functions/2-scope.cpp
+++ lecture_notes/09 functions/2-scope.cpp
@@ -2,41 +2,56 @@
 using namespace std;
 
 // Function that calculates len of each element in array
-void mainOutput() {
+void LengthElements() {
     string cities[] = {"Athens", "Roma", "London", "Beijing", "Kiev", "Riga"};
 
-    for (int i = 0; i < 5; i++) {
-      cout << cities[i].size() << " ";
+    for (int i = 0; i < 5; i++) { cout << cities[i].size() << " ";  }
+
+}
+
+// Functions were created function to improve the readability of code 
+
+void ToUpperFirst() {
+    string FirstCities[] = {"Athens", "Roma"};
+
+    for (auto & city : FirstCities) {
+        transform(city.begin(), city.end(), city.begin(), ::toupper);
+        cout << city << endl;
     }
+
+    cout << endl;
+
+}
+
+void ToLowerFirst() {
+    string FirstCities[] = {"Athens", "Roma"};
+
+    for (auto & city : FirstCities) {
+        transform(city.begin(), city.end(), city.begin(), ::tolower);
+        cout << city << endl;
+    }
+
+    cout << endl;
+
+}
+
+void ToUpperSecond() {
+    string ThirdCities[] = {"London", "Beijing", "Kiev"};
+
+    for (auto & city : ThirdCities) {
+        transform(city.begin(), city.end(), city.begin(), ::toupper);
+        cout << city << endl;
+    }
+
+    cout << endl;
 
 }
 
 int main() {
-    string FirstCities[] = {"Athens", "Roma"};
-
-    for (auto & citie : FirstCities) {
-        transform(citie.begin(), citie.end(), citie.begin(), ::toupper);
-        cout << citie << endl;
-    }
-
-    cout << endl;
-
-    for (auto & citie : FirstCities) {
-        transform(citie.begin(), citie.end(), citie.begin(), ::tolower);
-        cout << citie << endl;
-    }
-
-    cout << endl;
-
-    string ThirdCities[] = {"London", "Beijing", "Kiev"};
-
-    for (auto & citie : ThirdCities) {
-        transform(citie.begin(), citie.end(), citie.begin(), ::toupper);
-        cout << citie << endl;
-    }
-
-    cout << endl;
-    mainOutput();
+    ToUpperFirst();
+    ToLowerFirst();
+    ToUpperSecond();
+    LengthElements();
 
     return 0;
 }
