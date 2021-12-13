e634d36fc9dc8f427aa578c1386c78400363d939
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-13 22:26:59 +0200

    Updated

--- lecture_notes/09 functions/4-rest.cpp
+++ lecture_notes/09 functions/4-rest.cpp
@@ -6,10 +6,11 @@ void PrintArrays (int arg[], int length) {
         cout << arg[n] << " ";
 }
 
+// There is no point to continue translating, 'cause we have already declared a type.
+
 int main ()
 {
     int FirstArray[] = {1, 2, 3};
-    PrintArrays (FirstArray,3);
+    PrintArrays(FirstArray,3);
     return 0;
 }
-
