75495cbd9d68a34e4e87ebd37c7c63667f892b4e
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-13 13:36:41 +0200

    Update
    Co-authored-by: Timur Shemsedinov <timur.shemsedinov@gmail.com>

--- lecture_notes/09 functions/3-default.cpp
+++ lecture_notes/09 functions/3-default.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int maximum(int a = 0, int b = 0)
-{
-    if (a > b) {
-        return a;
-    }
-
-    if (b > a) {
-        return b;
-    }
-    return 0;
+int maximum(int a = 0, int b = 0) {
+    if (a > b) return a;
+    return b;
 }
 
 
