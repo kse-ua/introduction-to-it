aa1ab3d10758b321d1a6f8a01dac3a77de23dab4
Liza Machukha <94099022+istepmother@users.noreply.github.com>
2021-12-13 08:46:53 +0200

    Update 4-rest.cpp

--- lecture_notes/09 functions/4-rest.cpp
+++ lecture_notes/09 functions/4-rest.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 using namespace std;
-#include <stdarg.h>
-
-// this doesn't work at all
-//void catchRest(int length, ...) {
-//string result[length];
-//    for(int i = 0; i < 4; i++) {
-//        cout << result[length] << " ";
-//    }
-//  cout << endl;
-//}
-
-
 
 int main() {
-    // catchRest(1, 2, 3);
-    // trying to pretend it works like that
     cout << "Enter a number: ";
     int number;
     cin >> number;
@@ -26,5 +12,3 @@ int main() {
     cout << "The type of " << word << " is string";
     return 0;
 }
-
-
