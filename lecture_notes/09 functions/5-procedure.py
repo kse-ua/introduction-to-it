9fe49f41abfc3e11193e3485f17a8ce4e496b330
complectzero <ichirkov@kse.org.ua>
2021-12-13 09:38:44 +0200

    procedure in py

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,29 @@
+def add_function(a, b):
+    res = a + b
+    return res
+
+cache = {}
+
+def add_procedure(a, b):
+    key = f'{str(a)},{str(b)}'
+    if key in cache:
+        return cache[key]
+    res = a + b
+    cache[key] = res
+    return res
+
+
+
+print([
+  add_function(10, 20),
+  add_function(1, 2),
+  add_function(100, 20),
+  add_function(100, 200),
+])
+
+print([
+  add_procedure(10, 20),
+  add_procedure(1, 2),
+  add_procedure(100, 20),
+  add_procedure(100, 200),
+])
