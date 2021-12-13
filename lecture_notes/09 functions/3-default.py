7144ece094aa52909e13279658b2b4b893da969f
complectzero <ichirkov@kse.org.ua>
2021-12-13 09:21:07 +0200

    upgrade

--- lecture_notes/09 functions/3-default.py
+++ lecture_notes/09 functions/3-default.py
@@ -1,7 +1,6 @@
 def max(a = 0, b = 0):
     if a > b:
         return a
-    else:
-        return b
+    return b
         
-print([max(10, 20), max(10), max(-20)])
+print([max(20, 20), max(10), max(-20)])
