7a8009304baf0008e35bbfbe5d235dfa12b25fe4
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:11:05 +0200

    Revert "array-methods-js"
    This reverts commit 9499707613433ae9be7263cf10b15c3bdd8856bc.

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -5,12 +5,4 @@ const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 const first = ages[0];
 const last = ages[ages.length - 1];
 
-// removing/adding to the back
-ages.push(25);
-ages.pop();
-// removing/adding to the front
-ages.unshift(25);
-ages.shift();
-// reverse method 
-ages.reverse();
-console.log(ages)
+console.log({ first, last }); 
