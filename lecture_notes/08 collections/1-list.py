ad98291d00b84703a296f9767ad2a9cfe0a51e6d
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:11:05 +0200

    Revert "list-methods-py"
    This reverts commit fed9aa6541454b9806d543bcea3b1ca47f8f1c02.

--- lecture_notes/08 collections/1-list.py
+++ lecture_notes/08 collections/1-list.py
@@ -3,27 +3,4 @@ ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first = ages[0]
 last = ages[-1]
 
-print({ first, last })
-
-# list methods
-ages.append(25)
-ages.pop(-1)
-# list return to the original form
-# add to the top of list
-ages.insert(0, 25)
-ages.pop(0)
-# list return to the original form
-# del method
-del ages[-1]
-print(ages)
-
-names = ['Timur', 'Anna', 'Dmytro', 'Veronika']
-# alphabetical order ['Anna', 'Dmytro', 'Timur', 'Veronika'], not permanent change
-sorted_list = sorted(names)
-# reverse order ['Veronika', 'Timur', 'Dmytro', 'Anna']
-sorted_list.reverse()
-# alphabetical order ['Anna', 'Dmytro', 'Timur', 'Veronika'], permanent change
-names.sort()
-# permanent reverse
-names.sort(reverse=True)
-print(names)
+print({ first, last }) 
