a5c830f40451f3988d00fc08e8a5c86cfdda73be
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:11:05 +0200

    Revert "translate-union-into-py"
    This reverts commit ac0b64f8e3264d92c1a899044838c788df64f860.

--- lecture_notes/08 collections/a-union.js
+++ lecture_notes/08 collections/a-union.js
@@ -1,7 +1,21 @@
-union = lambda set1, set2: set1 | set2
-cities1 = {'Beijing', 'Kiev'}
-cities2 = {'Kiev', 'London', 'Baghdad'}
-print(f'cities1: {cities1}, cities2: {cities2}')
+'use strict';
 
-results = union(cities1, cities2)
-print(results)
+const union = (s1, s2) => {
+  const ds = s1.slice(0);
+  for (let i = 0; i < s2.length; i++) {
+    const item = s2[i];
+    if (!ds.includes(item)) ds.push(item);
+  }
+  return ds;
+};
+
+// const union = (s1, s2) => new Set([...s1, ...s2]);
+
+// Usage
+
+const cities1 = ['Beijing', 'Kiev'];
+const cities2 = ['Kiev', 'London', 'Baghdad'];
+console.dir({ cities1, cities2 });
+
+const results = union(cities1, cities2);
+console.dir(results);
