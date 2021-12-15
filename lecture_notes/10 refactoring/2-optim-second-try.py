105c594e6f48b764b3267cb29e41cb065f27bd1e
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-15 10:56:46 +0200

    Create 2-optim-second-try.py

---
+++ lecture_notes/10 refactoring/2-optim-second-try.py
@@ -0,0 +1,23 @@
+import json
+
+def parse(point):
+    point = json.loads(point)
+    return point
+
+def shift(offset, points):
+    for i in range(0, len(points), 1):
+        if type(points[i]) == str:
+            points[i] = parse(points[i])
+        points[i]['x'] += offset['x']
+        points[i]['y'] += offset['y']
+    return points
+
+polyline = [
+  { "x": 0, "y": 0 },
+  { "x": 10, "y": 10 },
+  '{ "x": 20, "y": 20 }',
+  { "x": 30, "y": 30 },
+]
+
+shift({'x': 10, 'y': -5}, polyline);
+print(polyline)
