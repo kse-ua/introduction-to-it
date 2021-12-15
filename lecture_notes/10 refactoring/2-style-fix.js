c7680a6baca4be9278117006730334304b4e73f6
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:54:02 +0200

    Create 2-style-fix.js
    let=>const, introduced variable for shift function, change the for cycle (I like it more this way)

---
+++ lecture_notes/10 refactoring/2-style-fix.js
@@ -0,0 +1,26 @@
+'use strict';
+const shift = (offset, points) => {
+  for (let point of points) {
+    const type = typeof point;
+    if (type === 'object') {
+      point.x += offset.x;
+      point.y += offset.y;
+    } else {
+      const i = points.indexOf(point);
+      points[i] = JSON.parse(point);
+      points[i].x += offset.x;
+      points[i].y += offset.y;
+    }
+  };
+  return points;
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = shift({ x: 10, y: -5 }, polyline);
+console.log({ polyline });
