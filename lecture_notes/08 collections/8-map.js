f3994e26abad7d5827564d32ba102d5bcb298e75
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:11:05 +0200

    Revert "translate-into-python"
    This reverts commit 06439fe8c48519f975b02deaa0d1cbb1ca33a26c.

--- lecture_notes/08 collections/8-map.js
+++ lecture_notes/08 collections/8-map.js
@@ -19,19 +19,3 @@ console.log({
 
 ages.clear();
 console.log({ ages });
-
-# Python version
-ages = {}
-
-ages["Vasia Pupkin"] = 19
-ages['Marcus Aurelius'] = 1860
-print(f"ages: {ages}")
-
-ages["Vasia Pupkin"] = 20
-print(f"ages: {ages}")
-
-print("Vasia Pupkin:", "Vasia Pupkin" in ages)
-print('Marcus Aurelius:', "Marcus Aurelius" in ages)
-
-ages.clear()
-print(f"ages: {ages}")
