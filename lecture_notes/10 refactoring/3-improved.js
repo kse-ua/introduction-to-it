11b8446b9b6d7f62bbd3355622b304e10ef8c519
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 10:04:37 +0200

    Create 3-improved.js
    introduced function for points

---
+++ lecture_notes/10 refactoring/3-improved.js
@@ -0,0 +1,26 @@
+'use strict';
+const shift = (offset, points) => {
+  const movedPoints = points.map((point) => {
+    const type = typeof point;
+    if (type === 'object') {
+      point.x += offset.x;
+      point.y += offset.y;
+    } else {
+      point = JSON.parse(point);
+      point.x += offset.x;
+      point.y += offset.y;
+    }
+    return point
+  });
+  return movedPoints;
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = shift({ x: 10, y: -5 }, polyline);
+console.log({ polyline });
