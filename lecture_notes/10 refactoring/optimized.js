0b930f2ec7ee27e26fd5bdaa0546ccfc69456440
complectzero <ichirkov@kse.org.ua>
2021-12-15 10:26:24 +0200

    style corrections

--- lecture_notes/10 refactoring/optimized.js
+++ lecture_notes/10 refactoring/optimized.js
@@ -1,19 +1,19 @@
 'use strict';
 
-let shift = (offset, points) => {
-  points.forEach((point) => {
+const shift = (offset, points) => {
+  const shifted = points.map((point) => {
     const type = typeof point;
     if (type === 'object') {
       point.x += offset.x;
       point.y += offset.y;
     } else {
-      let i = points.indexOf(point);
-      points[i] = JSON.parse(point);
-      points[i].x += offset.x;
-      points[i].y += offset.y;
+      point = JSON.parse(point);
+      point.x += offset.x;
+      point.y += offset.y;
     }
+    return point;
   });
-  return points;
+  return shifted;
 };
 
 const polyline = [
@@ -23,5 +23,5 @@ const polyline = [
   { x: 30, y: 30 },
 ];
 
-shift({ x: 10, y: -5 }, polyline);
-console.log({ polyline });
+const res = shift({ x: 10, y: -5 }, polyline);
+console.log ({ res });
