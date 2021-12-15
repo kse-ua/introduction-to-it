56fcf8b1ac74ade4d4e66c465aa92c0df92b747d
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 10:28:57 +0200

    Create 4-decomposition.js
    split the argument into different ones,  deleted movedPoints,  created a separate const for polyline.map(),

---
+++ lecture_notes/10 refactoring/4-decomposition.js
@@ -0,0 +1,24 @@
+'use strict';
+const shift = (offset) => (point) => {
+    const type = typeof point;
+    if (type === 'object') {
+      point.x += offset.x;
+      point.y += offset.y;
+    } else {
+        point = JSON.parse(point);
+        point.x += offset.x;
+        point.y += offset.y;
+    }
+    return point
+  };
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = shift({ x: 10, y: -5 });
+const shifted = polyline.map(offset)
+console.log({ shifted });
