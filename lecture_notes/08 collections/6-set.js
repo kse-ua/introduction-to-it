8c32b042f2ecdd2e533ee8acd50858bd8677694f
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-15 09:11:05 +0200

    Revert "translate-sets-into-python"
    This reverts commit edaeb8b967e1de3b5c99209aed3d00e06c53a370.

--- lecture_notes/08 collections/6-set.js
+++ lecture_notes/08 collections/6-set.js
@@ -1,11 +1,17 @@
-# python translation
+'use strict';
 
-ages = {10, 12, 15, 15, 17, 18, 18, 19, 20}
-print(ages)
+const ages = new Set([10, 12, 15, 15, 17, 18, 18, 19, 20]);
+console.log({ ages });
 
-ages.add(16)
-ages.remove(20)
-print(f"10: {10 in ages}\n16: {16 in ages}\n19: {19 in ages}\n20: {20 in ages}")
+ages.add(16);
+ages.delete(20);
 
-ages.clear()
-print(ages)
+console.log({
+  10: ages.has(10),
+  16: ages.has(16),
+  19: ages.has(19),
+  20: ages.has(20),
+});
+
+ages.clear();
+console.log({ ages });
