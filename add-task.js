3f55c90c0a48c901b98753fd723ddabe4620a7c3
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-15 15:10:20 +0200

    Create add-task.js

---
+++ add-task.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const curry = (fn) => (...args) => (
+  fn.length > args.length ? curry(
+    fn.bind(0, ...args)
+    ) : fn(...args)
+);
+
+// Usage
+
+const sum4 = (a, b, c, d) => (a + b + c + d);
+
+const f = curry(sum4);
+const y1 = sum4(1, 2, 3, 4);
+const y2 = f(1, 2, 3, 4);
+const y3 = f(1, 2, 3)(4);
+const y4 = f(1, 2)(3)(4);
+const y5 = f(1)(2)(3)(4);
+const y6 = f(1)(2, 3, 4);
+const y7 = f(1)(2)(3, 4);
+const y8 = f(1, 2)(3, 4);
+console.log(y1, y2, y3, y4, y5, y6, y7, y8);
