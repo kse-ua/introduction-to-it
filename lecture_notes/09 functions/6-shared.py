415622177a4aea34e640be314d402afeae41287e
Sofiia Zinchenko <69196200+Selbonyka@users.noreply.github.com>
2021-12-15 10:45:38 +0200

    Create 6-shared.py

---
+++ lecture_notes/09 functions/6-shared.py
@@ -0,0 +1,22 @@
+def add_procedure(a, b):
+    cache = {} # I've made cache local, so the results don't get mixed uo
+    key = str(a) + ', ' + str(b)
+    if key in cache:
+        return cache[key]
+    res = a + b
+    cache[key] = res
+    return res
+
+
+def sub_procedure(a, b):
+    cache = {}
+    key = str(a) + ', ' + str(b)
+    if key in cache:
+        return cache[key]
+    res = a - b
+    cache[key] = res
+    return res
+
+
+print(add_procedure(5, 2))
+print(sub_procedure(5, 2))
