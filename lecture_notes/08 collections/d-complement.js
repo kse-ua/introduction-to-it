c14832bd6f78f712f9a3bf2eade1d6c42140a6b2
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-16 17:39:10 +0200

    Delete d-complement.js

--- lecture_notes/08 collections/d-complement.js
+++
@@ -1,8 +0,0 @@
-difference = lambda a, b: a - b
-complement = lambda a, b: difference(a, b)
-
-cities1 = {'Beijing', 'Kiev'}
-cities2 = {'Kiev', 'London', 'Baghdad'}
-
-result = complement(cities2, cities1)
-print(result)
