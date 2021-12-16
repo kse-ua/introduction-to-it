828d67205965a049bb103f1b8d69235ba50e06b1
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-16 19:13:00 +0200

    Update 2-fix-style.js

--- lecture_notes/10 refactoring/2-fix-style.js
+++ lecture_notes/10 refactoring/2-fix-style.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const shift = (offset, points) => {
-  const shiftPoints = points.map((point) => {
-    const type = typeof point;
+  const shifted = points.map((dot) => {
+    const type = typeof dot;
     if (type === 'object') {
-      point.x += offset.x;
-      point.y += offset.y;
+      dot.x += offset.x;
+      dot.y += offset.y;
     } else {
-      point = JSON.parse(point);
-      point.x += offset.x;
-      point.y += offset.y;
+      dot = JSON.parse(dot);
+      dot.x += offset.x;
+      dot.y += offset.y;
     }
-    return point;
+    return dot;
   });
-  return shiftPoints;
+  return shifted;
 };
 
 const polyline = [
-    { x: 0, y: 0 },
-    { x: 10, y: 10 },
-    '{ "x": 20, "y": 20 }',
-    { x: 30, y: 30 },
-  ];
-  
-  const path = shift({ x: 10, y: -5 }, polyline);
-  console.log({ path });
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const path = shift({ x: 10, y: -5 }, polyline);
+console.log({ path });
