952bf2f13d64e1b3257b61033089841cae9e58e2
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-16 14:59:09 +0200

    Create 6-nearly-optimized.js
    separated the function into 2

---
+++ lecture_notes/10 refactoring/6-nearly-optimized.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const shift = (offset) =>  (point) => {
+  point.x += offset.x;
+  point.y += offset.y;
+  return point;
+};
+
+const conditional = (item) => {
+  if (typeof item === 'object') return item;
+  return JSON.parse(item);
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = shift({ x: 10, y: -5 });
+const parsed = polyline.map(conditional);
+const shifted = parsed.map(offset);
+console.log({ shifted });
