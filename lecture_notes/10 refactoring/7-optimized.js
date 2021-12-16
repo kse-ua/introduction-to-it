b4369fb081327f55a97d0e96ce906d89cdcd1658
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-16 15:03:07 +0200

    Create 7-optimized.js
    introduced const in shift function so that the function would not return initial argument, but the new result

---
+++ lecture_notes/10 refactoring/7-optimized.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const shift = (offset) =>  (point) => {
+  const shifted = {
+    x: point.x += offset.x,
+    y: point.y += offset.y,
+  };
+  return shifted;
+};
+
+const conditional = (item) => {
+  if (typeof item === 'object') return item;
+  return JSON.parse(item);
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = shift({ x: 10, y: -5 });
+const parsed = polyline.map(conditional);
+const shifted = parsed.map(offset);
+console.log({ shifted });
