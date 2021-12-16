0cea45c62e65081f9937da8a3105825df1033a28
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-16 21:00:54 +0200

    Create 7-clear.js

---
+++ lecture_notes/10 refactoring/7-clear.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const shift = (offset) => (dot) => {
+  const moved = {
+    x: dot.x += offset.dx,
+    y: dot.y += offset.dy,
+  };
+return moved;
+};
+
+const parsers = { string: JSON.parse, object: (obj) => obj };
+
+const conditionalParse = (item) => {
+  const parser = parsers[typeof item];
+  return parser(item);
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const move = shift({ dx: 10, dy: -5 });
+const parsed = polyline.map(conditionalParse);
+const path = parsed.map(move);
+console.log({ path });
