9c3124e168f87699311fa9ced90d48677997a426
Kate732 <kmaistrenko@kse.org.ua>
2021-12-16 20:55:41 +0200

    1-changed_py

--- lecture_notes/10 refactoring/1-optimal-mine.py
+++ lecture_notes/10 refactoring/1-optimal-mine.py
@@ -1,27 +1,27 @@
 import json
 
 def shift(offset, points):
-    for point in points:
+    for ind, point in enumerate(points):
         type_p = type(point)
         if type_p == dict:
-            point[x] += offset[x]
-            point[y] += offset[y]
+            point['x'] += offset['x']
+            point['y'] += offset['y']
         else:
-            i = points.index(point)
-            points[i] = json.dumps(point)
-            #print(point)
+            points[ind] = json.loads(point)
+            point = points[ind]
+            #print()
             #"""
-            point["x"] += offset[x]
-            point["y"] += offset[y]
+            point["x"] += offset['x']
+            point["y"] += offset['y']
             #"""
     return points
 
-x, y = 0, 0
+
 polyline = [
-  { x: 0, y: 0 },
-  { x: 10, y: 10 },
+  { 'x': 0, 'y': 0 },
+  { 'x': 10, 'y': 10 },
   '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 }
+  { 'x': 30, 'y': 30 }
 ]
 
-print(shift({ x: 10, y: -5 }, polyline))
+print(shift({ 'x': 10, 'y': -5 }, polyline))
