02bd5232a120170b92b612a6986dae5e67cea2ea
Anna Myslytska <94068423+amyslytska@users.noreply.github.com>
2021-12-16 15:41:38 +0200

    Create 1-not-optimal.py

---
+++ lecture_notes/10 refactoring/1-not-optimal.py
@@ -0,0 +1,23 @@
+import ast
+def shift(offset, points):
+    for point in points:
+        type_of = type(point)
+        if type_of != str:
+            point['x'] += offset['x']
+            point['y'] += offset['y']
+        else:
+            i = points.index(point)
+            points[i] = ast.literal_eval(point)
+            points[i]['x'] += offset['x']
+            points[i]['y'] += offset['y']
+
+    return points
+
+
+polyline = [{'x': 0, 'y': 0},
+            {'x': 10, 'y': 10},
+            '{ "x": 20, "y": 20 }',
+            {'x': 30, 'y': 30}]
+
+shift({'x': 10, 'y': -5}, polyline)
+print(polyline)
