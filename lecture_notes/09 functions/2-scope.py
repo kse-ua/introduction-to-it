b202c65726fd4028249b033a8ed554c7628b0dbe
Iryna Sukhoiarska <94047966+ISukhoiarska@users.noreply.github.com>
2021-12-17 00:12:47 +0200

    Update 2-scope.py

--- lecture_notes/09 functions/2-scope.py
+++ lecture_notes/09 functions/2-scope.py
@@ -1,27 +1,36 @@
 cities = ['Athens', 'Roma', 'London', 'Beijing', 'Kiev', 'Riga']
-f = lambda x: len(x)
+f = list(map(lambda x: len(x), cities))
+
 
 def upper_cities():
     old_cities = ['Athens', 'Roma']
-    f_1 = lambda x: x.upper()
+    f_1 = list(map(lambda x: x.upper(), old_cities))
     print({'cities': old_cities})
-    print(list(map(f_1, old_cities)))
+    print(f_1)
+
+
 upper_cities()
+
 
 def lower_cities():
     old_cities = ['Athens', 'Roma']
-    f_2 = lambda x: x.lower()
+    f_2 = list(map(lambda x: x.lower(), old_cities))
     print({'cities': old_cities})
-    print(list(map(f_2, old_cities)))
+    print(f_2)
+
+
 lower_cities()
 
 
 def last_cities():
     new_cities = ['London', 'Beijing', 'Kiev']
-    f_3 = lambda x: x.upper()
+    f_3 = list(map(lambda x: x.upper(), new_cities))
     print({'cities': new_cities})
-    print(list(map(f_3, new_cities)))
+    print(f_3)
+
+
 last_cities()
 
+
 print(cities)
-print(list(map(f, cities)))
+print(f)
