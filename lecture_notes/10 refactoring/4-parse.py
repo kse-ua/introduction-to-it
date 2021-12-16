38fb3fd5e36ba4ecd20f35089193da5e6ae335ec
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-16 22:59:41 +0200

    Update 4-parse.py

--- lecture_notes/10 refactoring/4-parse.py
+++ lecture_notes/10 refactoring/4-parse.py
@@ -1,10 +1,6 @@
 import json
 
 
-def conditional_parse(points):
-    return [json.loads(point) if type(point) == str else point for point in points]
-
-
 def shift(points, offset):
     for point in points:
         point['x'] += offset['x']
@@ -12,12 +8,18 @@ def shift(points, offset):
     return points
 
 
+def conditional_parse(item):
+    if type(item) == dict:
+        return item
+    return json.loads(item)
+
+
 polyline = [
-    {'x': 0, 'y': 0},
-    {'x': 10, 'y': 10},
-    '{ "x": 20, "y": 20 }',
-    {'x': 30, 'y': 30},
+  {'x': 0, 'y': 0},
+  {'x': 10, 'y': 10},
+  '{"x": 20, "y": 20}',
+  {'x': 30, 'y': 30},
 ]
 
-parsed = conditional_parse(polyline)
+parsed = list(map(conditional_parse, polyline))
 print(shift(parsed, {'x': 10, 'y': -5}))
