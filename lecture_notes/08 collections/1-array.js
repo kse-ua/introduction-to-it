1001d43de75a77ffb77337ab2c85e775ef666516
Stanislav Kramar <38153658+blackoutnet@users.noreply.github.com>
2021-12-16 03:53:22 +0200

    Revert "Stanislav Kramar lab 8"

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -2,12 +2,6 @@
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 
-ages.shift();
-ages.unshift(5);
-
-ages.pop();
-ages.push(5);
-
 const first = ages[0];
 const last = ages[ages.length - 1];
 
--- lecture_notes/08 collections/3-lambda.py
+++ lecture_notes/08 collections/3-lambda.py
@@ -1,14 +1,9 @@
 # Implementation
 
-get_first_and_last = lambda array: (array[0], array[-1])
+getFirstAndLast = lambda array: (array[0], array[-1])
 
 # Usage
 
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
-first, last = get_first_and_last(ages)
-print({'first': first, 'last': last})
-
-# While get_first_and_last lambda in python and getFirstAndLast function in the js version serve the same purpose,
-# their return types are not equivalent. The js version returns an object with two properties first and last.
-# In the python implementation, a tuple of 2 elements is returned. Also, python lambdas only allow to specify
-# a single expression to be returned while in js you can write any number of statements before return.
+first, last = getFirstAndLast(ages)
+print({ 'first': first, 'last': last })
