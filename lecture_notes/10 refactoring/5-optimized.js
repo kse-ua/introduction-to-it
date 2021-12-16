0130ea018a6968ffc642714bbd48dc5d5c9aece6
Veronika Nikiforova <90610237+vnikiforova@users.noreply.github.com>
2021-12-16 20:48:56 +0200

    Create 5-optimized.js

---
+++ lecture_notes/10 refactoring/5-optimized.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const shift = (offset) => (dot) => {
+  dot.x += offset.x;
+  dot.y += offset.y;
+  return dot;
+};
+
+const conditionalParse = (object) => 
+  (typeof object === 'string' ? JSON.parse(object) : object);
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const move = shift({ x: 10, y: -5 });
+const parsed = polyline.map(conditionalParse);
+const path = parsed.map(move);
+console.log({ path });
