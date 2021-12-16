a05ae0bd14d367b4342dd1f61544b516b1dc4a9b
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-16 15:16:31 +0200

    Create curry.js

---
+++ lecture_notes/10 refactoring/curry.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Curry takes a func and then return curried
+// Checking if n. arg (fn = sum4 ) > inputted args (y2, y3..) currying.. if n.args == fn, just put args in fn
+
+const curry = (fn) => (...args) => {
+    return fn.length > args.length ? curry(fn.bind(this, ...args)) : fn(...args);
+};
+
+const sum4 = (a, b, c, d) => (a + b + c + d);
+
+const f = curry(sum4);
+
+const y1 = sum4(1, 2, 3, 4);
+const y2 = f(1, 2, 3, 4);
+const y3 = f(1, 2, 3)(4);
+const y4 = f(1, 2)(3)(4);
+const y5 = f(1)(2)(3)(4);
+const y6 = f(1)(2, 3, 4);
+const y7 = f(1)(2)(3, 4);
+const y8 = f(1, 2)(3, 4);
+console.log(y1, y2, y3, y4, y5, y6, y7, y8);
