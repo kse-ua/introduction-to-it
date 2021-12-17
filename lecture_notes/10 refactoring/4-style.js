3cec01ef68b6ed1f8d1d93888290811e76fa0966
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-17 10:54:45 +0200

    Update 4-style.js

--- lecture_notes/10 refactoring/4-style.js
+++ lecture_notes/10 refactoring/4-style.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let parse_point = (point) => {
-    return (typeof point === 'object' ? point: JSON.parse(point))
+    return (typeof point === 'object' ? point: JSON.parse(point));
 }
 
 let shift = (offset) => (point) => {
@@ -17,7 +17,7 @@ const polyline = [
     { x: 30, y: 30 },
 ];
 
-const to_offset = {x: 10, y: -5}
-const parsed = polyline.map(parse_point)
-const result = parsed.map(shift(to_offset))
-console.log({ result })
+const to_offset = {x: 10, y: -5};
+const parsed = polyline.map(parse_point);
+const result = parsed.map(shift(to_offset));
+console.log({ result });
