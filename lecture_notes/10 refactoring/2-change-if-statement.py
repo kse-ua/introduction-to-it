89ea74967e3f8f886847ebf30e6ae05adb48e27e
Liza Machukha <94099022+istepmother@users.noreply.github.com>
2021-12-17 07:12:11 +0200

    Update 2-change-if-statement.py

--- lecture_notes/10 refactoring/2-change-if-statement.py
+++ lecture_notes/10 refactoring/2-change-if-statement.py
@@ -1,9 +1,9 @@
 import json
 
+
 def shift(offset, points):
     for point in points:
-        type_of = str(type(point))
-        if type_of != "<class 'dict'>":
+        if type(point) != dict:
             i = points.index(point)
             point = json.loads(point)
             points[i] = point
@@ -11,12 +11,13 @@ def shift(offset, points):
         point["y"] += offset["y"]
     return points
 
+
 polyline = [
-    {'x': 0, 'y': 0},
-    {'x': 10, 'y': 10},
+    {"x": 0, "y": 0},
+    {"x": 10, "y": 10},
     '{ "x": 20, "y": 20 }',
-    {'x': 30, 'y': 30},
+    {"x": 30, "y": 30},
 ]
 
-shift({'x': 10, 'y': -5}, polyline)
-print(f'polyline: {polyline}')
+shift({"x": 10, "y": -5}, polyline)
+print(f"polyline: {polyline}")
