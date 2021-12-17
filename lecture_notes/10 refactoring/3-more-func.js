494bad33a13d9290e5ab42470a18ad9b3c5075f3
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2021-12-17 11:12:41 +0200

    Update 3-more-func.js

--- lecture_notes/10 refactoring/3-more-func.js
+++ lecture_notes/10 refactoring/3-more-func.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
-let parse_point = (point) => {
-    if (typeof point === 'object') {
-        return point;
-    } else {
-        return JSON.parse(point);
-    }
-}
+let parsePoint = (point) => {
+  if (typeof point === "object") {
+    return point;
+  } else {
+    return JSON.parse(point);
+  }
+};
 
 let shift = (offset) => (point) => {
-    point.x += offset.x;
-    point.y += offset.y;
-    return point;
+  point.x += offset.x;
+  point.y += offset.y;
+  return point;
 };
 
 const polyline = [
-    { x: 0, y: 0 },
-    { x: 10, y: 10 },
-    '{ "x": 20, "y": 20 }',
-    { x: 30, y: 30 },
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
 ];
 
-const to_offset = {x: 10, y: -5};
-const parsed = polyline.map(parse_point);
-parsed.map(shift(to_offset));
+const toOffset = { x: 10, y: -5 };
+const parsed = polyline.map(parsePoint);
+parsed.map(shift(toOffset));
 console.log({ parsed });
