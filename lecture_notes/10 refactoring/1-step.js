a5e9bcc163877f2f3a9ee289ff0ecd943ea77b65
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-17 23:41:53 +0200

    Update 1-step.js

--- lecture_notes/10 refactoring/1-step.js
+++ lecture_notes/10 refactoring/1-step.js
@@ -1,1 +1,28 @@
+'use strict';
 
+const shift = (offset, points) => {
+//Here we create new set using map
+    const moving = points.map((point) => {
+        const type = typeof point;
+        if (type === 'object') {
+            point.x += offset.x;
+            point.y += offset.y;
+        } else {
+            point = JSON.parse(point);
+            point.x += offset.x;
+            point.y += offset.y;
+        }
+        return point;
+    });
+    return moving;
+};
+
+const polyline = [
+    {x: 0, y: 0},
+    {x: 10, y: 10},
+    '{ "x": 20, "y": 20 }',
+    {x: 30, y: 30},
+];
+
+const path = shift({x: 50, y: -40}, polyline);
+console.log({path});
