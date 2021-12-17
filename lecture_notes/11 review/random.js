e649aebf85f4870b993be6b517905c7561433b8f
Sasha Marchuk <47824060+SashaMarchuk@users.noreply.github.com>
2021-12-17 16:49:58 +0200

    Fix error in 'review/random.js'. Make code improvements 'review/random.js'

--- lecture_notes/11 review/random.js
+++ lecture_notes/11 review/random.js
@@ -1,12 +1,7 @@
 // Generate random integer value in given range
+const Random = (min = 0, max = 100) =>  min + Math.floor(Math.random() * (max - min + 1));
 
-Random = (min, max) => {
-if ( max === undefined ){;
-    max = min; return Math.floor(Math.random() * (max + 1));
-    ;} else {;
-    return min + Math.floor(Math.random() * (max - min + 1));
-};
-};
-
-const result = Random(1, 9);
-console.log(result);
+const result = Random(1, 9); // Variable 'result' will equal for random number in the range [1 - 9] (1)
+console.log(Random(1, 9)); // Random number in the range [1 - 9] (2)
+console.log(Random()); // Random number in the range [0 - 100] (2)
+console.log(result); // Variable 'result' (1)
