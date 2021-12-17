3727331d1e6bc4c921e70c035930e4bd86c86e7c
Bohdan Kulynych <31761679+Bohdan-Kulynych@users.noreply.github.com>
2021-12-17 18:37:49 +0200

    Fix errors. Refactor generate password function.
    I've used right approach with variable declaration, added more expected results to the function. Also added default value for function's arguments.

--- lecture_notes/11 review/password.js
+++ lecture_notes/11 review/password.js
@@ -1,14 +1,15 @@
 // Generate random password
 
-let GeneratePassword = ( alphabet , length ) => {
-  const MAX = alphabet.length
-  key = '';
+const GeneratePassword = (alphabet = 'abc123', length = 7) =>  {
+  let key = '';
   for ( let i = 0; i < length; i++ ){
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    const Index = Math.floor(Math.random() * alphabet?.length);
+    key += alphabet?.[Index];
   };
   return key
-}
+};
 
-const result = GeneratePassword('abc123', 7);
-console.log(result);
+const result = GeneratePassword('abc123', 7); // Random password for 7 characters using 'abc123' (1)
+console.log(Random('abc', 9)); // Random password for 9 characters using 'abc'
+console.log(Random()); // Random password for 7 characters using 'abc123'
+console.log(result); // Variable 'result' (1)
