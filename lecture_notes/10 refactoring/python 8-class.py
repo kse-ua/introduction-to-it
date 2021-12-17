0d32b0c2a1c07559bf203301b98d8b4e691b3d1c
aratushna <94103037+aratushna@users.noreply.github.com>
2021-12-17 09:33:36 +0200

    Create python 8-class.py

---
+++ lecture_notes/10 refactoring/python 8-class.py
@@ -0,0 +1,36 @@
+import ast  # abstract syntax tree, conversion to code "{'x': 20,'y': 20}" >> {'x': 20,'y': 20}
+
+
+class Shift:
+    def __init__(self, point):  # python __init__ = javascript constructor
+        self.point = point  # python self = javascript this
+        self.x = list(point.values())[0]  # point['x'] >> self.x
+        self.y = list(point.values())[1]  # point['y'] >> self.y
+        self.dx = self.x
+        self.dy = self.y
+
+    def move(self, point):
+        return {'x': point.x + self.dx, 'y': point.y + self.dy}  # self >> offset, self.x >> offset.dx ...
+
+
+def parsers(point, x):
+    # returns a dictionary, "{'x': 20,'y': 20}" >> {'x': 20,'y': 20}
+    _point = ast.literal_eval(point) if x else point  # ternary operator
+    return Shift(_point)   # create an object
+
+def conditionalParse(point):
+    parser = parsers(point, (str(type(point)) == "<class 'str'>"))
+    return parser
+
+
+polyline = [
+    {'x': 0, 'y': 0},
+    {'x': 10, 'y': 10},
+    "{'x': 20,'y': 20}",
+    {'x': 30, 'y': 30},
+]
+print('polyline :', '\n', polyline)
+shift = Shift({'dx': 10, 'dy': -5})  # create an object
+polyline = list(map(conditionalParse, polyline))
+path = list(map(shift.move, polyline))
+print('path :', '\n', path)
