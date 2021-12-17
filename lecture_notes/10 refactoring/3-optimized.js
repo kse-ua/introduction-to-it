812b0f634630cce7a9901541e489d003a01a87f3
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-17 11:00:43 +0200

    Update 3-optimized.js

--- lecture_notes/10 refactoring/3-optimized.js
+++ lecture_notes/10 refactoring/3-optimized.js
@@ -21,11 +21,9 @@ const shift = (points) => {
   return mooved;
 };
 
-const polyline = [
-  { x: 0, y: 0 },
-  { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 },
-];
+polyline = [{'x': 0, 'y': 0},
+             {'x': 10, 'y': 10},
+             '{ "x": 20, "y": 20 }',
+             {'x': 30, 'y': 30}]
 
 console.log(shift(polyline));
