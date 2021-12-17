302ba5e3fd28cd82dafec5f5cea2f5cf529ac208
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-17 22:20:53 +0200

    Create 2-function.js

---
+++ lecture_notes/10 refactoring/2-function.js
@@ -0,0 +1,29 @@
+function shift(offset, points) {
+    points.forEach(function (point) {
+        point.x += offset.x
+        point.y += offset.y
+    });
+    return points;
+}
+
+function parser(points) {
+    points.forEach(
+        function (point) {
+            if (typeof (point) === "string") {
+                var i = points.indexOf(point)
+                points[i] = JSON.parse(points[i])
+            }
+        });
+    return points;
+}
+
+var polyline = [
+    {x: 0, y: 0},
+    {x: 10, y: 10},
+    '{"x": 20, "y": 20}',
+    {x: 30, y: 30}
+]
+
+polyline = parser(polyline)
+shift({x: 10, y: -5}, polyline)
+console.log(polyline)
