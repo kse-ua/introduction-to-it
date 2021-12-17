bab73bb0b75ab2a675ce27c8174198bfe201d493
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-17 22:20:12 +0200

    Create 4-string.py

---
+++ lecture_notes/10 refactoring/4-string.py
@@ -0,0 +1,29 @@
+import json
+
+
+def shift(offset, points):
+    for point in points:
+         point['x'] += offset['x']
+         point['y'] += offset['y']
+    return points
+
+
+def parser(point):
+    if type(point) is dict:
+        return point
+    return json.loads(point)
+
+
+polyline = [
+    {'x': 0 ,'y': 0},
+    {'x': 10, 'y': 10},
+    '{"x": 20, "y": 20}',
+    {'x': 30, 'y': 30}
+]
+
+polyline = list(map(parser, polyline))
+shift({'x': 10, 'y': -5}, polyline)
+for i in polyline:
+    print(i)
+
+
