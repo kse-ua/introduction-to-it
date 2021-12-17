0e48e1ea4234570ca3867e2b111cf1087f555f39
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-12-17 23:37:05 +0200

    Create 3-map.js

---
+++ lecture_notes/10 refactoring/3-map.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function shift(offset, points) {
+    points.forEach(function (point) {
+        point.x += offset.x;
+        point.y += offset.y;
+    });
+    return points;
+}
+
+function parser(point){
+    if(typeof(point) == 'string')
+        return JSON.parse(point);
+    return point;
+}
+
+var polyline = [
+    {x: 0, y: 0},
+    {x: 10, y: 10},
+    '{"x": 20, "y": 20}',
+    {x: 30, y: 30}
+]
+
+polyline = polyline.map(function(e) {
+    e = parser(e);
+    return e;
+});
+
+shift({x: 10, y: -5}, polyline);
+console.log(polyline);
