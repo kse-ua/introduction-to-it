72296fa234d3caf878cf17997aeac59ee57050ca
istepmother <imachukha@kse.org.ua>
2021-12-17 10:50:52 +0200

    first improvement

---
+++ lecture_notes/10 refactoring/2-change-if-statement.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const shift = (offset, points) => {
+  const moved = points.map((point) => {
+    const type = typeof point;
+    if (type !== 'object') {
+      point = JSON.parse(point);
+    }
+    point.x += offset.x;
+    point.y += offset.y;
+
+    return point;
+  });
+  return moved;
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const offset = { x: 10, y: -5 };
+console.log(shift(offset, polyline));
