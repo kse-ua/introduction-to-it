56994181a5bb14efb742c8a17465243a91685cd2
Iryna Sukhoiarska <94047966+ISukhoiarska@users.noreply.github.com>
2021-12-17 09:06:32 +0200

    Update 4-rest.py

--- lecture_notes/09 functions/4-rest.py
+++ lecture_notes/09 functions/4-rest.py
@@ -1,10 +1,15 @@
-def catchRest(*args):
+def catch_rest(*args):
     print(list(args))
-catchRest(1, 3, 5)
+    
+    
+catch_rest(1, 3, 5)
+
 
 def f2(*args):
     for x in args:
         arg_type = type(x)
         print(f"Type: {arg_type}")
         print(f"Value: {x}")
+        
+        
 f2(1, 'Marcus', {'field': 'value'})
