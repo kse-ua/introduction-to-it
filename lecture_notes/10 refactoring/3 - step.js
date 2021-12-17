f789b1bfb19860018c84e42f3422fb97c1a8fbe0
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-18 00:13:36 +0200

    Create 3 - step.js

---
+++ lecture_notes/10 refactoring/3 - step.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const move =
+    ({ dx, dy }) =>
+      ({ x, y }) =>
+        ({
+//Returning result
+            x: x + dx,
+            y: y + dy,
+        });
+
+const conditionalParse = (item) =>
+//Cheking for 'True' or 'False'
+    (
+        typeof item === 'string'
+        ? JSON.parse(item)
+        : item
+    );
+
+const polyline = [
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 },
+];
+
+const offset = move({ dx: 45, dy: 54 });
+const parsed = polyline.map(conditionalParse);
+const path = parsed.map(offset);
+console.log({ path });
