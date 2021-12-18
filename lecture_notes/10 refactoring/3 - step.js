1fc54c5fe636696e67f31f9ddba500479565c916
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-18 02:37:15 +0200

    Update 3 - step.js

--- lecture_notes/10 refactoring/3 - step.js
+++ lecture_notes/10 refactoring/3 - step.js
@@ -2,26 +2,26 @@
 
 const move =
     ({ dx, dy }) =>
-      ({ x, y }) =>
-        ({
-          //Returning result
-          x: x + dx,
-          y: y + dy,
-        });
+        ({ x, y }) =>
+            ({
+                //Returning result
+                x: x + dx,
+                y: y + dy,
+            });
 
 const coordParse = (item) =>
 //Cheking for 'True' or 'False'
-  (
-    typeof item === 'string'
-      JSON.parse(item) ?
-      item :
-  );
+    (
+        typeof item === 'string' ?
+JSON.parse(item) :
+    item
+);
 
 const polyline = [
-  { x: 0, y: 0 },
-  { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 },
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 },
 ];
 
 const offset = move({ dx: 45, dy: 54 });
