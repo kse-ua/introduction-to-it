bd82bbe11a6730fbbeb4c415235d4581255d7050
Kate732 <kmaistrenko@kse.org.ua>
2021-12-18 13:31:09 +0200

    2-js

---
+++ lecture_notes/10 refactoring/2-fixed-style.js
@@ -0,0 +1,27 @@
+  'use strict';
+
+const shift = (offset, points) => {
+    const moved = points.map((point) => {
+      const type = typeof point;
+      if (type === 'object') {
+        point.x += offset.x;
+        point.y += offset.y;
+      } else {
+        point = JSON.parse(point);
+        point.x += offset.x;
+        point.y += offset.y;
+      }
+      return point;
+    });
+    return moved;
+  };
+  
+  const polyline = [
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 },
+  ];
+  
+  const path = shift({ x: 10, y: -5 }, polyline);
+  console.log({ path });
