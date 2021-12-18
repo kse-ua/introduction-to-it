87f3f9d7c00f526f0d41731ecdefac15d6054876
Denis <94136440+DenisMilkevich@users.noreply.github.com>
2021-12-18 02:05:39 +0200

    Create 3 - step.py

---
+++ lecture_notes/10 refactoring/3 - step.py
@@ -0,0 +1,27 @@
+import json
+
+
+def shift(coords):
+    def move(point):
+        point['x'] += coords['x']
+        point['y'] += coords['y']
+        return point
+
+    return move
+
+
+def parse_point(point):
+    return point if (type(point) is dict) else json.loads(point)
+
+
+polyline = [
+    {'x': 0, 'y': 0},
+    {'x': 10, 'y': 10},
+    '{"x": 20, "y": 20}',
+    {'x': 30, 'y': 30}
+]
+offset = {'x': 45, 'y': 54}
+parsed = list(map(parse_point, polyline))
+path = list(map(shift(offset), parsed))
+for obj in path:
+    print(obj)
