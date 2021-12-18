9f6f4e8318f0af26ebcc67f69797bee3b393fb0c
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-18 23:47:35 +0200

    Update 1-step-new.cpp

--- lecture_notes/10 refactoring/1-step-new.cpp
+++ lecture_notes/10 refactoring/1-step-new.cpp
@@ -1,7 +1,10 @@
 # include <iostream>
 # include <string>
 # include <map>
+# include "nlohmann.h"
+# include <typeinfo>
 
+using json = nlohmann::json;
 using namespace std;
 
 enum Type {
@@ -15,18 +18,20 @@ struct PolylineEntry {
     Type type;
 };
 
-//void Parse(PolylineEntry polylines[], int size) {
-//    for (int i = 0; i < size; i++) {
-//        if (polylines->type == Dictionary) {
-//            //
-//        }
-//        else if (polylines->type == String) {
-//            //
-//        }
-//    }
-//}
+void Parse(PolylineEntry polylines[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (polylines[i].type == String) {
+            auto jsonParsed = json::parse(polylines[i].str);
+            polylines[i].dict = {
+                    {"x", jsonParsed["x"]},
+                    {"y", jsonParsed["y"]}
+            };
+            polylines[i].type = Dictionary;
+        }
+    }
+}
 
-int main() {
+int addingValues(const int arr[]) {
     map<string, int> polyline[] = {
             {
                     {"x", 0},
@@ -89,12 +94,33 @@ int main() {
             entry3,
     };
 
+    Parse(polylines, 4);
+
     for (auto & poly : polylines) {
         for (const auto &p : poly.dict) {
             cout << "m[" << p.first << "] = " << p.second << '\n';
         }
     }
+    cout << "\n" << "Adding new values ..." << endl << "\n";
+    for (auto & poly : polylines) {
+        for (const auto &p: poly.dict) {
+            if( p.first == "x") {
+                int a = p.second + arr[0];
+                cout << "m[" << a << "] = " << a << '\n';
+            }
+            else if ( p.first == "y") {
+                int a = p.second + arr[1];
+                cout << "m[" << a << "] = " << a << '\n';
+            }
+
+        }
+    }
 
     return 0;
 
+}
+
+int main() {
+    int arr[] = {10, -5};
+    addingValues(arr);
 }
