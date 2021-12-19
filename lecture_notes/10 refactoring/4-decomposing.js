59494dd43b55204f7b37df288c8e69fc6a8a140a
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-19 05:44:18 +0100

    Create 4-decomposing.js
    Decomposed the big main function (from the previously fixed codes) into 2 smaller ones ("shift" and "elementType" functions)

---
+++ lecture_notes/10 refactoring/4-decomposing.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// declaring a separate function for checking a type of "point"
+const elementType = (point) => {
+  if (typeof point === 'object') 
+     return point;
+  else 
+     return JSON.parse(point);
+};
+
+const shift = (offset) => (point) => {
+  point.x += offset.x;
+  point.y += offset.y;
+  return point;
+};
+
+const polyline = [
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
+];
+
+const type = polyline.map(elementType); /* the function "elementType" is
+    executed for EVERY line of "polyline" because the "map() method" is used */
+const newShift = type.map( shift({ x: 10, y: -5 }) );
+console.log({ newShift });
