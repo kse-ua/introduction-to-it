153a77cc67964e28a4b6f61d74942c6c8ed69adf
Kate732 <kmaistrenko@kse.org.ua>
2021-12-21 20:38:30 +0200

    3-js

---
+++ lecture_notes/10 refactoring/3-fixed_decompose.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const addTwoObj = (addend, point) => {
+    point.x += addend.x;
+    point.y += addend.y;
+    return point;
+}
+
+const addingArrAndObj = (addend, points) => {
+    points.forEach((point) => {
+        const type = typeof point;
+        if (type === 'object') {
+            point = addTwoObj(addend, point);
+        } else {
+            point = JSON.parse(point);
+            point.x += addend.x;
+            point.y += addend.y;
+        }
+    });
+    return points;
+};
+
+const arrOfObj = [
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 },
+];
+
+addingArrAndObj({ x: 10, y: -5 }, arrOfObj);
+console.log({ arrOfObj });
