2611b64129e346169c310e82e0c354e078ab63cf
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-22 13:53:46 +0200

    Update 3-optimized.js

--- lecture_notes/10 refactoring/3-optimized.js
+++ lecture_notes/10 refactoring/3-optimized.js
@@ -1,31 +1,21 @@
 'use strict';
-
-const offset = { x: 10, y: -5 };
-
 const moove = (point) => {
   point.x += offset.x;
   point.y += offset.y;
   return point;
 };
 
-const parse = (point) => {
-  if (typeof point === 'object') {
-    return point;
-  }
-  return JSON.parse(point);
-};
-
-const shift = (points) => {
-  const parsed = points.map(parse);
-  const mooved = parsed.map(moove);
-  return mooved;
-};
+const parse = (point) => (typeof point === 'object') ? point : JSON.parse(point);
 
 const polyline = [
   { x: 0, y: 0 },
   { x: 10, y: 10 },
   '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 },
+  { x: 30, y: 30 }
 ];
+const offset = { x: 10, y: -5 };
 
-console.log(shift(polyline));
+const parsed = polyline.map(parse);
+const mooved = parsed.map(parsed => moove(parsed, offset));
+console.log(mooved);
+
