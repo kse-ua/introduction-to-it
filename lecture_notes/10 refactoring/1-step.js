7301d03f5937a98919bd5a6c4fcfb7e0c971385c
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-22 12:29:58 +0200

    Update 1-step.js

--- lecture_notes/10 refactoring/1-step.js
+++ lecture_notes/10 refactoring/1-step.js
@@ -1,32 +1,30 @@
 'use strict';
 
-// Function that iterates each element in polyline checks if it's an obj; parse it using JSON.
-const point_parse = (points) => {
+const pointParse = (points) => {
     return points.map((point) => {
-        if (typeof point != 'object') {
+        if (typeof point !== 'object') {
             return JSON.parse(point);
         } else {
-            return point
+            return point;
         }
     });
-}
+};
 
 // Taking a result from PointParse and shifting it
-const points_shift = (arr, dot_param) => {
+const pointsShift = (arr, dotParam) => {
     return arr.map((dot) => {
-        dot['x'] += dot_param['x']
-        dot['y'] += dot_param['y']
+        dot['x'] += dotParam['x'];
+        dot['y'] += dotParam['y'];
         return dot;
     });
-}
+};
 
 const polyline = [
-    { x: 0, y: 0 },
-    { x: 10, y: 10 },
-    '{ "x": 20, "y": 20 }',
-    { x: 30, y: 30 },
+  { x: 0, y: 0 },
+  { x: 10, y: 10 },
+  '{ "x": 20, "y": 20 }',
+  { x: 30, y: 30 },
 ];
 
-
-const path = point_parse(polyline)
-console.log(points_shift(path, { x: 10, y: -5 }))
+const path = pointParse(polyline);
+console.log(pointsShift(path, { x: 10, y: -5 }));
