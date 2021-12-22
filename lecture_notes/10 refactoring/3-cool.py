0caa4d7325d683d04230f2103ea3abd8f090fd26
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-22 12:47:29 +0200

    Create 3-cool.py

---
+++ lecture_notes/10 refactoring/3-cool.py
@@ -0,0 +1,32 @@
+import json
+
+def parse(point):
+     point = json.loads(point)
+     
+     return point
+
+
+def shift(offset, points):
+     for i in range(0, len(points), 1):
+         if type(points[i]) == str:
+             points[i] = parse(points[i])
+         move(points[i], offset)
+     
+     return points
+
+
+def move(point,offset):
+     point['x'] += offset['x']
+     point['y'] += offset['y']
+
+
+polyline = [
+   { "x": 0, "y": 0 },
+   { "x": 10, "y": 10 },
+  '{ "x": 20, "y": 20 }',
+   { "x": 30, "y": 30 },
+ ]
+ 
+
+shift({'x': 10, 'y': -5}, polyline)
+print(polyline)
