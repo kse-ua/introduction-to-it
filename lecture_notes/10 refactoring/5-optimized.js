bef079beccea51491f84f0bee2c5de427329bba3
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-22 10:21:34 +0100

    Update 5-optimized.js
    Replaced an "if..else" statement (from the previous code) in "elementType" function with a ternary operator & passed the actual arguments to the "shift" function (instead of "offset" and "point")

--- lecture_notes/10 refactoring/5-optimized.js
+++ lecture_notes/10 refactoring/5-optimized.js
@@ -1,10 +1,11 @@
 'use strict';
 
- //a ternary operator here
+//a ternary operator here
 const elementType = (point) =>
   (typeof point === 'object' ? point : JSON.parse(point));
-  
-const shift = ({ coordinate1, coordinate2 }) => ({ x, y }) => ({ x: x + coordinate1, y: y + coordinate2 });
+
+const shift = ({ coordinate1, coordinate2 }) => 
+({ x, y }) => ({ x: x + coordinate1, y: y + coordinate2 });
 
 const polyline = [
   { x: 0, y: 0 },
