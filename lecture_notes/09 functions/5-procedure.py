b0fcde924889339a43204a384b92546e35d43b96
Artem Ginkul <artem@Artems-MacBook-Pro.local>
2021-12-22 11:54:31 +0200

    translate to python

---
+++ lecture_notes/09 functions/5-procedure.py
@@ -0,0 +1,97 @@
+def add(a, b):
+    return a + b
+def cached_add(a, b, cache):
+
+
+
+    key = f'{a},{b}'
+
+
+
+    cached_result = cache.get(key)
+
+
+
+    if cached_result is None:
+
+
+
+        result = a + b
+
+
+
+        cache[key] = result
+
+
+
+    else:
+
+
+
+        result = cached_result
+
+    return result
+
+
+
+
+
+
+
+
+
+
+
+if name == '__main__':
+
+
+
+    def main():
+
+
+
+        cache = {}
+
+
+
+
+
+
+
+        print([add(10, 20), add(1, 2), add(100, 20), add(100, 200)])
+
+
+
+        print([
+
+
+
+            cached_add(10, 20, cache),
+
+
+
+            cached_add(1, 2, cache),
+
+
+
+            cached_add(100, 20, cache),
+
+
+
+            cached_add(100, 200, cache)
+
+
+
+        ])
+
+
+
+
+
+
+
+
+
+
+
+    main()
