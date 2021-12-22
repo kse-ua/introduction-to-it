e27f1796ab7e4ad7a3e70a86ec22f04e8b203f6f
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-22 10:31:04 +0100

    Update 6-enhanced.js
    Added a new dictionary-function "actionsType" to determine a type of an element and run a function corresponding to this type. Consequently, the "elementType" function is written differently now.

--- lecture_notes/10 refactoring/6-enhanced.js
+++ lecture_notes/10 refactoring/6-enhanced.js
@@ -1,22 +1,25 @@
 'use strict';
 
 const actionsType = {
-    string: (element) => JSON.parse(element),
-    object: (element) => element,
-};  
+  string: (element) => JSON.parse(element),
+  object: (element) => element,
+};
 
-/* called this new function (which is actually a dictionary) "actionsType" 
-cuz it describes actions that need to be done with the element depending on its type.
+/* called this new function (which is actually a dictionary) "actionsType"
+cuz it describes actions that need to be done 
+with the element depending on its type.
 This dictionary stores element's type as 'key',
 and actions on this element(function) as its 'value' */
 
- const elementType = (point) => actionsType[typeof point](point); 
- /* "actionsType[typeof point](point)"  is a function which firstly takes point's type ("actionsType[typeof point]") 
- and then returns a function depending on the type of this point, taking point as an argument */
+ const elementType = (point) => actionsType[typeof point](point);
+ /* "actionsType[typeof point](point)"  is a function
+ which firstly takes point's type ("actionsType[typeof point]")
+ and then returns a function depending on
+ the type of this point, taking point as an argument */
 
-const shift = ({ coordinate1, coordinate2 }) => ({ x, y }) => ({ 
-    x: x + coordinate1, 
-    y: y + coordinate2, 
+const shift = ({ coordinate1, coordinate2 }) => ({ x, y }) => ({
+    x: x + coordinate1,
+    y: y + coordinate2,
 });
 
 const polyline = [
@@ -27,5 +30,5 @@ const polyline = [
 ];
 
 const type = polyline.map(elementType);
-const newShift = type.map( shift({ coordinate1: 10, coordinate2: -5 }) );
+const newShift = type.map(shift({ coordinate1: 10, coordinate2: -5 }));
 console.log({ newShift });
