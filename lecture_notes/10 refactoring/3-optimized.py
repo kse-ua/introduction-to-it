4481619143f0af156b94efc21714a152ece9cf02
Andrew Trybushnyi <94055869+roflmyrlok@users.noreply.github.com>
2021-12-22 13:51:56 +0200

    Update 3-optimized.py

--- lecture_notes/10 refactoring/3-optimized.py
+++ lecture_notes/10 refactoring/3-optimized.py
@@ -1,26 +1,25 @@
 import json
 
 def parse(point):
-    point = json.loads(point)
-    return point
+    return json.loads(point) if type(point) == str else point
 
-def shift(offset, points):
-    for i in range(0, len(points), 1):
-        if type(points[i]) == str:
-            points[i] = parse(points[i])
-        moove(points[i], offset)
-    return points
-
-def moove(point,offset):
+def moove(point, offset):
     point['x'] += offset['x']
     point['y'] += offset['y']
+    return point
+
+def shift(points, offset):
+    parsedPoints = list(map(lambda point: parse(point), points))
+    moovedPoints = list(map(lambda point: moove(point, offset), parsedPoints))
+    return moovedPoints
+
 
 polyline = [
-  { "x": 0, "y": 0 },
-  { "x": 10, "y": 10 },
-  '{ "x": 20, "y": 20 }',
-  { "x": 30, "y": 30 },
+    {"x": 0, "y": 0},
+    {"x": 10, "y": 10},
+    '{ "x": 20, "y": 20 }',
+    {"x": 30, "y": 30}
 ]
+offset = {'x': 10, 'y': -5}
 
-shift({'x': 10, 'y': -5}, polyline)
-print(polyline)
+print(shift(polyline, offset))
