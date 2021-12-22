4546bff207c5086fdcf7aa028905d68a5ddd7086
Ann Ramazanova <94097590+Ann-the-programmer@users.noreply.github.com>
2021-12-22 10:18:20 +0100

    Update 4-decomposing.js
    Decomposed the big main function (from the previous codes) into 2 smaller ones ("shift" and "elementType" functions)

--- lecture_notes/10 refactoring/4-decomposing.js
+++ lecture_notes/10 refactoring/4-decomposing.js
@@ -2,10 +2,10 @@
 
 // declaring a separate function for checking a type of "point"
 const elementType = (point) => {
-  if (typeof point === 'object') 
-     return point;
-  else 
-     return JSON.parse(point);
+  if (typeof point === 'object')
+    return point;
+  else
+    return JSON.parse(point);
 };
 
 const shift = (offset) => (point) => {
