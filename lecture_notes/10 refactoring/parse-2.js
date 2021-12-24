07d3218d22a49341b5169d7faccddfe7a6a86334
Mark Lastovskyi <emloft@protonmail.com>
2021-12-24 11:35:27 +0200

    updated arrow function usage

--- lecture_notes/10 refactoring/parse-2.js
+++ lecture_notes/10 refactoring/parse-2.js
@@ -3,15 +3,15 @@
 const shift = ({ a, b }) => ({ x, y }) => ({ x: x + a, y: y + b });
 
 const checkType = (line) =>
-  (typeof line !== 'object' ? JSON.parse(line) : line);
+   { return (typeof line !== 'object' ? JSON.parse(line) : line); };
 
 const offset = shift({ a: 10, b: -5 });
 
 const polyline = [
-  { x: 0, y: 0 },
-  { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 },
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 }
 ];
 
 const checkedType = polyline.map(checkType);
--- lecture_notes/10 refactoring/parse.js
+++ lecture_notes/10 refactoring/parse.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const shift = (offset, points) => {
-  for (const point of points) {
-    const type = typeof point;
-    if (type === 'object') {
-      point.x += offset.x;
-      point.y += offset.y;
-    } else {
-      const i = points.indexOf(point);
-      points[i] = JSON.parse(point);
-      points[i].x += offset.x;
-      points[i].y += offset.y;
+    for (const point of points) {
+        const type = typeof point;
+        if (type === 'object') {
+            point.x += offset.x;
+            point.y += offset.y;
+        } else {
+            const i = points.indexOf(point);
+            points[i] = JSON.parse(point);
+            points[i].x += offset.x;
+            points[i].y += offset.y;
+        }
     }
-  }
 
-  return points;
+    return points;
 };
 
 const polyline = [
-  { x: 0, y: 0 },
-  { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
-  { x: 30, y: 30 },
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 }
 ];
 
 shift({ x: 10, y: -5 }, polyline);
