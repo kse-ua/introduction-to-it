ffcac01d6d0eb1dadd31485bce55c92d5b4d24ad
Ilya Chirkov <94166996+complectzero@users.noreply.github.com>
2021-12-24 09:49:17 +0200

    python version

---
+++ lecture_notes/10 refactoring/optimized.py
@@ -0,0 +1,25 @@
+import json
+
+def parse(point):
+    if str(type(point)) != "<class 'dict'>":
+        point = json.loads(point)
+    return point
+
+
+def shift(point, offset):
+    point["x"] += offset["x"]
+    point["y"] += offset["y"]
+    return point
+
+polyline = [
+  {"x": 0, "y": 0},
+  {"x": 10, "y": 10},
+  '{"x": 20, "y": 20}',
+  {"x": 30, "y": 30},
+]
+
+offset = { "x": 10, "y": -5 }
+
+parsed = list(map(parse, polyline))
+shifted = list(map(lambda point: shift( point , offset) , parsed))
+print(shifted)
